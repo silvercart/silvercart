0448ef079dd274ac15a70fe4f7017c4c985bd7ac
Sebastian Diel <sdiel@pixeltricks.de>
2011-01-27 19:14:37 +0100

    Seitentyp FrontPage wieder eingefügt;

---
+++ code/pages/FrontPage.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * represents a shopping cart. Every customer has one initially.
+ *
+ * @author Roland Lehmann <rlehmann@pixeltricks.de>
+ * @copyright Pixeltricks GmbH
+ * @license BSD
+ * @since 23.10.2010
+ */
+class FrontPage extends Page {
+
+    public static $singular_name = "Frontpage";
+    public static $plural_name = "Frontpages";
+
+   
+
+}
+/**
+ * related controller
+ *
+ * @author Roland Lehmann <rlehmann@pixeltricks.de>
+ * @since 23.10.2010
+ * @license BSD
+ * @copyright 2010 pixeltricks GmbH
+ */
+class FrontPage_Controller extends Page_Controller {
+
+}
