cb9340bef25d5fb2a4d8f9619a7a1e91a2b8d855
Sascha Koehler <skoehler@pixeltricks.de>
2011-01-31 15:56:53 +0100

    Class Carrier ffollows the coding standards now.

--- code/order/Carrier.php
+++ code/order/Carrier.php
@@ -1,28 +1,76 @@
 <?php
-
 /**
  * abstract for a shipping carrier
  *
  * @author Roland Lehmann <rlehmann@pixeltricks.de>
  * @copyright Pixeltricks GmbH
  * @since 06.11.2010
- * @license BSD
+ * @license none
  */
 class Carrier extends DataObject {
 
+    /**
+     * Singular name
+     *
+     * @var string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 31.01.2011
+     */
     public static $singular_name = "Frachtführer";
+
+    /**
+     * Plural name
+     *
+     * @var string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 31.01.2011
+     */
     public static $plural_name = "Frachtführer";
+
+    /**
+     * Attributes.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 31.01.2011
+     */
     public static $db = array(
-        'Title' => 'VarChar(25)',
-        'FullTitle' => 'VarChar(60)'
+        'Title'             => 'VarChar(25)',
+        'FullTitle'         => 'VarChar(60)'
     );
+
+    /**
+     * Has-many relationship.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 31.01.2011
+     */
     public static $has_many = array(
-        'shippingMethods' => 'ShippingMethod',
-        'zones' => 'Zone'
+        'shippingMethods'   => 'ShippingMethod',
+        'zones'             => 'Zone'
     );
+
+    /**
+     * Column labels for display in tables.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @copyright 2011 pixeltricks GmbH
+     * @since 31.01.2011
+     */
     public static $field_labels = array(
-        'Title' => 'Name',
-        'FullTitle' => 'voller Name'
+        'Title'             => 'Name',
+        'FullTitle'         => 'voller Name'
     );
 
     /**
@@ -34,15 +82,14 @@ class Carrier extends DataObject {
      */
     public function requireDefaultRecords() {
         parent::requireDefaultRecords();
+
         if (!DataObject::get('Carrier')) {
             $carrier = new Carrier();
             $carrier->Title = 'DHL';
             $carrier->FullTitle = 'DHL International GmbH';
             $carrier->write();
 
-            /**
-             * the carrier DHL has at least one shipping method
-             */
+            // the carrier DHL has at least one shipping method
             if (!DataObject::get('ShippingMethod')) {
                 $shippingMethod = new ShippingMethod();
                 $shippingMethod->Title = 'Paket';
@@ -52,6 +99,4 @@ class Carrier extends DataObject {
             }
         }
     }
-
 }
-
