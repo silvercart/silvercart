9a6473d4a028c4aaf4e654ad2337c1a8e72f2d87
Sascha Koehler <koehler@standardized.de>
2011-03-15 22:52:36 +0100

    Fixed error in SilvercartContactForm: mail was sent to user instead of shop admin.

--- code/custom_forms/SilvercartContactForm.php
+++ code/custom_forms/SilvercartContactForm.php
@@ -106,19 +106,23 @@ class SilvercartContactForm extends CustomHtmlForm {
      */
     protected function fillInFieldValues() {
         $this->formFields['Salutation']['title'] = _t('SilvercartAddress.SALUTATION');
-        $this->formFields['Salutation']['value'] = array('' => _t('SilvercartEditAddressForm.EMPTYSTRING_PLEASECHOOSE'), "Frau" => _t('SilvercartAddress.MISSIS'), "Herr" => _t('SilvercartAddress.MISTER'));
-        $this->formFields['FirstName']['title'] = _t('SilvercartAddress.FIRSTNAME', 'firstname');
-        $this->formFields['Surname']['title'] = _t('SilvercartAddress.SURNAME');
-        $this->formFields['Email']['title'] = _t('SilvercartAddress.EMAIL', 'email address');
-        $this->formFields['Message']['title'] = _t('SilvercartPage.MESSAGE', 'message');
-        $this->preferences['submitButtonTitle'] = _t('SilvercartPage.SUBMIT_MESSAGE', 'submit message');
+        $this->formFields['Salutation']['value'] = array(
+            ''      => _t('SilvercartEditAddressForm.EMPTYSTRING_PLEASECHOOSE'),
+            "Frau"  => _t('SilvercartAddress.MISSIS'),
+            "Herr"  => _t('SilvercartAddress.MISTER')
+        );
+        $this->formFields['FirstName']['title']  = _t('SilvercartAddress.FIRSTNAME', 'firstname');
+        $this->formFields['Surname']['title']    = _t('SilvercartAddress.SURNAME');
+        $this->formFields['Email']['title']      = _t('SilvercartAddress.EMAIL', 'email address');
+        $this->formFields['Message']['title']    = _t('SilvercartPage.MESSAGE', 'message');
+        $this->preferences['submitButtonTitle']  = _t('SilvercartPage.SUBMIT_MESSAGE', 'submit message');
 
         $member = Member::currentUser();
         if ($member) {
             $this->formFields['Salutation']['selectedValue'] = $member->Salutation;
-            $this->formFields['FirstName']['value'] = $member->FirstName;
-            $this->formFields['Surname']['value'] = $member->Surname;
-            $this->formFields['Email']['value'] = $member->Email;
+            $this->formFields['FirstName']['value']          = $member->FirstName;
+            $this->formFields['Surname']['value']            = $member->Surname;
+            $this->formFields['Email']['value']              = $member->Email;
         }
     }
 
@@ -135,22 +139,21 @@ class SilvercartContactForm extends CustomHtmlForm {
      * @return void
      */
     protected function submitSuccess($data, $form, $formData) {
-
         $email = new Email(
-                        Email::getAdminEmail(),
-                        $formData['Email'],
-                        _t('SilvercartContactFormPage.REQUEST', 'request via contact form'),
-                        ''
+            Email::getAdminEmail(),
+            Email::getAdminEmail(),
+            _t('SilvercartContactFormPage.REQUEST', 'request via contact form'),
+            ''
         );
 
         $email->setTemplate('SilvercartMailContact');
         $email->populateTemplate(
-                array(
-                    'FirstName' => $formData['FirstName'],
-                    'Surname' => $formData['Surname'],
-                    'Email' => $formData['Email'],
-                    'Message' => str_replace('\r\n', '<br>', nl2br($formData['Message']))
-                )
+            array(
+                'FirstName' => $formData['FirstName'],
+                'Surname'   => $formData['Surname'],
+                'Email'     => $formData['Email'],
+                'Message'   => str_replace('\r\n', '<br>', nl2br($formData['Message']))
+            )
         );
 
         $email->send();
@@ -160,5 +163,4 @@ class SilvercartContactForm extends CustomHtmlForm {
         $contactFormResponsePage = SilvercartPage_Controller::PageByIdentifierCode("SilvercartContactFormResponsePage");
         Director::redirect($contactFormResponsePage->RelativeLink());
     }
-
 }
