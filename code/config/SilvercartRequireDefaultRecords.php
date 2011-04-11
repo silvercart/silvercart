55a49933f86dbf35ae8e38a991f4d992aeea1043
Sebastian Diel <sdiel@pixeltricks.de>
2011-04-11 17:35:02 +0200

    MINOR: countries will be generated before carrier now (on /dev/build);

--- code/config/SilvercartRequireDefaultRecords.php
+++ code/config/SilvercartRequireDefaultRecords.php
@@ -120,6 +120,9 @@ class SilvercartRequireDefaultRecords extends DataObject {
             $silvercartConfig->write();
         }
 
+        // create countries
+        $this->requireOrUpdateCountries();
+
         //create a carrier and an associated zone and shipping method
         $carrier = DataObject::get_one('SilvercartCarrier');
         if (!$carrier) {
@@ -148,8 +151,8 @@ class SilvercartRequireDefaultRecords extends DataObject {
             if (!$country) {
                 $country = new SilvercartCountry();
                 $country->Title = 'Deutschland';
-                $country->ISO2 = 'de';
-                $country->ISO3 = 'deu';
+                $country->ISO2 = 'DE';
+                $country->ISO3 = 'DEU';
                 $country->write();
                 $zoneDomestic->SilvercartCountries()->add($country);
             }
@@ -235,9 +238,6 @@ class SilvercartRequireDefaultRecords extends DataObject {
                 $obj->write();
             }
         }
-
-        // create countries
-        $this->requireOrUpdateCountries();
 
         // create number ranges
         $orderNumbers = DataObject::get('SilvercartNumberRange', "`Identifier`='OrderNumber'");
