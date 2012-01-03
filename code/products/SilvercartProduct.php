03345347dc519f07e6188c5c11cf8f1536a76693
Sascha Koehler <skoehler@pixeltricks.de>
2012-01-03 12:21:23 +0100

    BUGFIX method 'get' in 'SilvercartProduct' checks for PriceGrossAmount again.

--- code/products/SilvercartProduct.php
+++ code/products/SilvercartProduct.php
@@ -434,7 +434,7 @@ class SilvercartProduct extends DataObject {
                     if ($pricetype == "net") {
                         $filter .= sprintf("(`isFreeOfCharge` = 1 OR `PriceNetAmount` != 0.0) AND ");
                     } else {
-                        $filter .= sprintf("(`isFreeOfCharge` = 1 OR `PriceNetAmount` != 0.0) AND ");
+                        $filter .= sprintf("(`isFreeOfCharge` = 1 OR `PriceGrossAmount` != 0.0) AND ");
                     }
                 } else {
                     $filter .= sprintf("`%s` !='' AND ", $requiredAttribute);
