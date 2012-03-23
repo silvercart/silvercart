7a8e0001e84f69dc264e485560a2068922762ed6
Sebastian Diel <sdiel@pixeltricks.de>
2012-03-23 10:38:17 +0100

    BUGFIX: fixed wrong CustomHtmlForm preferences extension in SilvercartPluginObjectExtension

--- code/plugins/SilvercartPluginObjectExtension.php
+++ code/plugins/SilvercartPluginObjectExtension.php
@@ -182,16 +182,15 @@ class SilvercartPluginObjectExtension extends DataObjectDecorator {
      * 
      * @return bool
      * 
-     * @author Sascha Koehler <skoehler@pixeltricks.de>
-     * @since 28.11.2011
+     * @author Sascha Koehler <skoehler@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 23.03.2012
      */
     public function updatePreferences(&$preferences) {
-        $preferences = SilvercartPlugin::call($this->owner, 'updatePreferences', $preferences, true, array());
+        $extendedPreferences = SilvercartPlugin::call($this->owner, 'updatePreferences', $preferences, true, array());
         
-        if ($preferences &&
-            is_array($preferences) &&
-            count($preferences) > 0) {
-            
+        if ($extendedPreferences &&
+            is_array($extendedPreferences) &&
+            count($extendedPreferences) > 0) {
             $preferences = $preferences[0];
         }
     }
