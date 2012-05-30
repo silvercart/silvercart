4244dbc6a65d3dbf39f95d71007263589144641f
Sascha Koehler <skoehler@pixeltricks.de>
2012-05-30 20:33:48 +0200

    ENHANCEMENT Slidorion widget is fully configurable now.

--- code/widgets/widget_silvercart_slidorion/SilvercartSlidorionProductGroupWidget.php
+++ code/widgets/widget_silvercart_slidorion/SilvercartSlidorionProductGroupWidget.php
@@ -35,6 +35,23 @@
 class SilvercartSlidorionProductGroupWidget extends SilvercartWidget {
     
     /**
+     * Attributes
+     *
+     * @var array
+     * 
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public static $db = array(
+        'widgetHeight' => 'Int',
+        'speed'        => 'Int',
+        'interval'     => 'Int',
+        'hoverPause'   => 'Boolean',
+        'autoPlay'     => 'Boolean',
+        'effect'       => "Enum('fade,slideLeft,slideRight,slideUp,slideDown,overLeft,overRight,overUp,overDown', 'fade')"
+    );
+    
+    /**
      * 1:1 or 1:n relationships.
      *
      * @var array
@@ -118,12 +135,13 @@ class SilvercartSlidorionProductGroupWidget extends SilvercartWidget {
      */
     public function getCMSFields() {
         $fields = new FieldSet();
-        $rootTabSet                 = new TabSet('Root');
-        $basicTab                   = new Tab('Basic', $this->fieldLabel('BasicTab'));
-        $translationTab             = new Tab('Translations', $this->fieldLabel('TranslationsTab'));
+        $rootTabSet     = new TabSet('Root');
+        $basicTab       = new Tab('Basic', $this->fieldLabel('BasicTab'));
+        $advancedTab    = new Tab('Advanced', $this->fieldLabel('AdvancedTab'));
+        $translationTab = new Tab('Translations', $this->fieldLabel('TranslationsTab'));
         
-        $titleField            = new TextField('FrontTitle',               $this->fieldLabel('FrontTitle'));
-        $contentField          = new TextareaField('FrontContent',         $this->fieldLabel('FrontContent'), 10);
+        $titleField   = new TextField('FrontTitle',               $this->fieldLabel('FrontTitle'));
+        $contentField = new TextareaField('FrontContent',         $this->fieldLabel('FrontContent'), 10);
         
         $productGroupHolder = SilvercartTools::PageByIdentifierCode('SilvercartProductGroupHolder');
         $productGroupDropdown = new SilvercartTreeMultiselectField(
@@ -138,15 +156,49 @@ class SilvercartSlidorionProductGroupWidget extends SilvercartWidget {
             'SilvercartSlidorionProductGroupWidgetLanguages',
             'SilvercartSlidorionProductGroupWidgetLanguage'
         );
+        $widgetHeightField = new TextField(
+            'widgetHeight',
+            $this->fieldLabel('widgetHeight')
+        );
+        $speedField = new TextField(
+            'speed',
+            $this->fieldLabel('speed')
+        );
+        $intervalField = new TextField(
+            'interval',
+            $this->fieldLabel('interval')
+        );
+        $effectField = new DropdownField(
+            'effect',
+            $this->fieldLabel('effect'),
+            singleton('SilvercartSlidorionProductGroupWidget')->dbObject('effect')->enumValues(),
+            $this->effect
+        );
+        $hoverPauseField = new CheckboxField(
+            'hoverPause',
+            $this->fieldLabel('hoverPause')
+        );
+        $autoPlayField = new CheckboxField(
+            'autoPlay',
+            $this->fieldLabel('autoPlay')
+        );
         
+        $basicTab->push($productGroupDropdown);
         $basicTab->push($titleField);
         $basicTab->push($contentField);
-        $basicTab->push($productGroupDropdown);
+
+        $advancedTab->push($widgetHeightField);
+        $advancedTab->push($speedField);
+        $advancedTab->push($intervalField);
+        $advancedTab->push($effectField);
+        $advancedTab->push($hoverPauseField);
+        $advancedTab->push($autoPlayField);
         
         $translationTab->push($translationsTableField);
         
         $fields->push($rootTabSet);
         $rootTabSet->push($basicTab);
+        $rootTabSet->push($advancedTab);
         $rootTabSet->push($translationTab);
         
         return $fields;
@@ -196,12 +248,186 @@ class SilvercartSlidorionProductGroupWidget extends SilvercartWidget {
      */
     public function fieldLabels($includerelations = true) {
         $fieldLabels = array(
-            'BasicTab'          => _t('SilvercartSlidorionProductGroupWidget.CMS_BASICTABNAME'),
-            'TranslationsTab'   => _t('SilvercartConfig.TRANSLATIONS'),
+            'SCProductGroupPages' => _t('SilvercartSlidorionProductGroupWidget.SCPRODUCTGROUPPAGES'),
+            'BasicTab'            => _t('SilvercartSlidorionProductGroupWidget.CMS_BASICTABNAME'),
+            'AdvancedTab'         => _t('SilvercartSlidorionProductGroupWidget.CMS_ADVANCEDTABNAME'),
+            'TranslationsTab'     => _t('SilvercartConfig.TRANSLATIONS'),
+            'FrontTitle'          => _t('SilvercartSlidorionProductGroupWidget.FRONT_TITLE'),
+            'FrontContent'        => _t('SilvercartSlidorionProductGroupWidget.FRONT_CONTENT'),
+            'widgetHeight'        => _t('SilvercartSlidorionProductGroupWidget.WIDGET_HEIGHT'),
+            'speed'               => _t('SilvercartSlidorionProductGroupWidget.SPEED'),
+            'interval'            => _t('SilvercartSlidorionProductGroupWidget.INTERVAL'),
+            'hoverPause'          => _t('SilvercartSlidorionProductGroupWidget.HOVERPAUSE'),
+            'autoPlay'            => _t('SilvercartSlidorionProductGroupWidget.AUTOPLAY'),
+            'effect'              => _t('SilvercartSlidorionProductGroupWidget.EFFECT'),
         );
 
         $this->extend('updateFieldLabels', $fieldLabels);
         return $fieldLabels;
+    }
+
+    /**
+     * Returns the widget height.
+     *
+     * @return int
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getWidgetHeightValue() {
+        $widgetHeight = 400;
+
+        if (!empty($this->widgetHeight)) {
+            $widgetHeight = (int) $this->widgetHeight;
+        }
+
+        if ($widgetHeight == 0) {
+            $widgetHeight = 400;
+        }
+
+        return $widgetHeight;
+    }
+
+    /**
+     * Returns the animation speed.
+     *
+     * @return int
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getSpeedValue() {
+        $speed = 1000;
+
+        if (!empty($this->speed)) {
+            $speed = (int) $this->speed;
+        }
+
+        if ($speed == 0) {
+            $speed = 1000;
+        }
+
+        return $speed;
+    }
+
+    /**
+     * Returns the interval speed.
+     *
+     * @return int
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getIntervalValue() {
+        $interval = 6000;
+
+        if (!empty($this->interval)) {
+            $interval = (int) $this->interval;
+        }
+
+        if ($interval == 0) {
+            $interval = 6000;
+        }
+
+        return $interval;
+    }
+
+    /**
+     * Returns whether to pause on hover as boolean string
+     *
+     * @return string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getHoverPauseValue() {
+        $hoverPause = 'false';
+
+        if (!empty($this->hoverPause)) {
+            $hoverPause = $this->hoverPause;
+
+            if ($hoverPause) {
+                $hoverPause = 'true';
+            } else {
+                $hoverPause = 'false';
+            }
+        }
+
+        return $hoverPause;
+    }
+
+    /**
+     * Returns whether to autoplay as boolean string
+     *
+     * @return string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getAutoPlayValue() {
+        $autoPlay = 'false';
+
+        if (!empty($this->autoPlay)) {
+            $autoPlay = $this->autoPlay;
+
+            if ($autoPlay) {
+                $autoPlay = 'true';
+            } else {
+                $autoPlay = 'false';
+            }
+        }
+
+        return $autoPlay;
+    }
+
+    /**
+     * Returns the effect type
+     *
+     * @return string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getEffectValue() {
+        $effect = 'fade';
+
+        if (!empty($this->effect)) {
+            $effect = $this->effect;
+        }
+
+        return $effect;
+    }
+
+    /**
+     * Returns the group picture list as HTML string.
+     *
+     * @return string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getGroupPictureList() {
+        $list = '';
+
+        foreach ($this->SCProductGroupPages() as $SCProductGroupPage) {
+            $list .= '<div class="silvercart-slidorion-slide">';
+            $list .= $SCProductGroupPage->GroupPicture()->SetRatioSize(426, $this->getSliderHeight());
+            $list .= '</div>';
+        }
+
+        return $list;
+    }
+
+    /**
+     * Returns the slider height.
+     *
+     * @return int
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 30.05.2012
+     */
+    public function getSliderHeight() {
+        return $this->getWidgetHeightValue() - 15;
     }
     
     /**
@@ -263,19 +489,24 @@ class SilvercartSlidorionProductGroupWidget_Controller extends SilvercartWidget_
                 "
                 (function($) {jQuery(document).ready(function(){
                     $('#silvercart-slidorion-%d').slidorion({
-                        speed:      1000,
-                        interval:   6000,
-                        effect:     'fade',
-                        hoverPause: true,
-                        autoPlay:   true
+                        speed:      %d,
+                        interval:   %d,
+                        effect:     '%s',
+                        hoverPause: %s,
+                        autoPlay:   %s
                     });
                 })})(jQuery);
                 ",
-                $this->ID
+                $this->ID,
+                $this->getSpeedValue(),
+                $this->getIntervalValue(),
+                $this->getEffectValue(),
+                $this->getHoverPauseValue(),
+                $this->getAutoPlayValue()
             )
         );
         
-        $slidorionHeight        = 400;
+        $slidorionHeight        = $this->getWidgetHeightValue();
         $numberOfItems          = $this->SCProductGroupPages()->Count();
         $accordeonTitleHeight   = 30;
         $correctionHeight       = 16;
@@ -284,10 +515,25 @@ class SilvercartSlidorionProductGroupWidget_Controller extends SilvercartWidget_
         Requirements::customCSS(
             sprintf(
                 "
+                #silvercart-slidorion-%d.silvercart-widget-slidorion-productgroup-slider {
+                    height: %dpx;
+                }
+                #silvercart-slidorion-%d .silvercart-slidorion-slider {
+                    height: %dpx;
+                }
+                #silvercart-slidorion-%d .silvercart-slidorion-accordeon {
+                    height: %dpx;
+                }
                 #silvercart-slidorion-%d .silvercart-slidorion-accordeon > .silvercart-slidorion-link-content {
                     height: %dpx;
                 }
                 ",
+                $this->ID,
+                $this->getWidgetHeightValue(),
+                $this->ID,
+                $this->getSliderHeight(),
+                $this->ID,
+                $this->getWidgetHeightValue(),
                 $this->ID,
                 $accordeonContentHeight
             )
--- templates/Widgets/SilvercartSlidorionProductGroupWidget.ss
+++ templates/Widgets/SilvercartSlidorionProductGroupWidget.ss
@@ -9,13 +9,7 @@
 
         <div id="silvercart-slidorion-{$ID}" class="silvercart-widget-slidorion-productgroup-slider">
             <div class="silvercart-slidorion-slider">
-                <% control SCProductGroupPages %>
-                    <div class="silvercart-slidorion-slide">
-                        <% control GroupPicture %>
-                            $SetRatioSize(426,385)
-                        <% end_control %>
-                    </div>
-                <% end_control %>
+                $getGroupPictureList
             </div>
 
             <div class="silvercart-slidorion-accordeon">
