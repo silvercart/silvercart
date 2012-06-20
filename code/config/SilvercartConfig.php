248c17c02622e6912dc6e7b5656a6de301477464
Sebastian Diel <sdiel@pixeltricks.de>
2012-06-20 16:33:50 +0200

    MINOR: changed SilvercartConfig default for the update version

--- code/config/SilvercartConfig.php
+++ code/config/SilvercartConfig.php
@@ -166,7 +166,7 @@ class SilvercartConfig extends DataObject {
      */
     public static $defaults = array(
         'SilvercartVersion'             => '1.3',
-        'SilvercartUpdateVersion'       => '1',
+        'SilvercartUpdateVersion'       => '3',
         'DefaultPriceType'              => 'gross',
         'GeoNamesActive'                => false,
         'GeoNamesAPI'                   => 'http://api.geonames.org/',
