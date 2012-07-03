07ee0de9b10bbed9356809e4a7aea7988e7bef00
Patrick Schneider <pschneider@pixeltricks.de>
2012-07-03 16:48:38 +0200

    BUGFIX: fixed a redirect bug in the last Checkoutstep

--- code/custom_forms/checkout/SilvercartCheckoutFormStep5.php
+++ code/custom_forms/checkout/SilvercartCheckoutFormStep5.php
@@ -98,8 +98,10 @@ class SilvercartCheckoutFormStep5 extends SilvercartCheckoutFormStepPaymentInit 
                 (!Member::currentUser()->SilvercartShoppingCart()->isFilled() &&
                  !array_key_exists('orderId', $checkoutData))) {
 
-                $frontPage = SilvercartPage_Controller::PageByIdentifierCode();
-                Director::redirect($frontPage->RelativeLink());
+                if (!Director::redirected_to()) {
+                    $frontPage = SilvercartPage_Controller::PageByIdentifierCode();
+                    Director::redirect($frontPage->RelativeLink());
+                }
             }
         }
     }
