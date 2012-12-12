2ad44767973b5e17ac2289c906365b6c802fbade
Sebastian Diel <sdiel@pixeltricks.de>
2012-12-12 13:44:39 +0100

    BUGFIX: fillInRequestValues of SilvercartContactForm will be called properly now

--- code/custom_forms/SilvercartContactForm.php
+++ code/custom_forms/SilvercartContactForm.php
@@ -104,10 +104,11 @@ class SilvercartContactForm extends CustomHtmlForm {
      * logged in users get there fields filled
      *
      * @return void
-     * @author Roland Lehmann <rlehmann@pixeltricks.de>
-     * @since 21.10.2010
+     * @author Roland Lehmann <rlehmann@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 12.12.2012
      */
     protected function fillInFieldValues() {
+        parent::fillInFieldValues();
         $this->formFields['Salutation']['title'] = _t('SilvercartAddress.SALUTATION');
         $this->formFields['Salutation']['value'] = array(
             ''      => _t('SilvercartEditAddressForm.EMPTYSTRING_PLEASECHOOSE'),
@@ -121,7 +122,9 @@ class SilvercartContactForm extends CustomHtmlForm {
         $this->preferences['submitButtonTitle']  = _t('SilvercartPage.SUBMIT_MESSAGE', 'submit message');
 
         $member = Member::currentUser();
-        if ($member) {
+        if ($member &&
+            (!array_key_exists('value', $this->formFields['FirstName']) ||
+             empty($this->formFields['FirstName']['value']))) {
             $this->formFields['Salutation']['selectedValue'] = $member->Salutation;
             $this->formFields['FirstName']['value']          = $member->FirstName;
             $this->formFields['Surname']['value']            = $member->Surname;
