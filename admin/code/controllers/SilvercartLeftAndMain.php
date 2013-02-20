db4015e81781de3e5975f8ca34e64e7011de1889
Sebastian Diel <sdiel@pixeltricks.de>
2013-02-20 18:01:19 +0100

    ENHANCEMENT: added SilvercartLeftAndMain; modified SilvercartModelAdmin;

--- admin/code/controllers/SilvercartLeftAndMain.php
+++ admin/code/controllers/SilvercartLeftAndMain.php
@@ -32,14 +32,7 @@
  * @since 19.02.2013
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
-class SilvercartModelAdmin extends ModelAdmin {
-    
-    /**
-     * Name of DB field to make records sortable by.
-     *
-     * @var string
-     */
-    public static $sortable_field = '';
+class SilvercartLeftAndMain extends LeftAndMain {
     
     /**
      * Provides hook for decorators, so that they can overwrite css
@@ -64,31 +57,12 @@ class SilvercartModelAdmin extends ModelAdmin {
      *
      * @return string 
      * 
-     * @author Roland Lehmann <rlehmann@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
      * @since 19.02.2013
      */
     public function SectionTitle() {
-        return _t($this->sanitiseClassName($this->modelClass) . '.PLURALNAME');
+        return _t($this->modelClass . '.SINGULARNAME');
     }
     
-    /**
-     * Builds and returns the edit form.
-     * 
-	 * @param int       $id     The current records ID. Won't be used for ModelAdmins.
-	 * @param FieldList $fields Fields to use. Won't be used for ModelAdmins.
-     * 
-     * @return Form
-     */
-    public function getEditForm($id = null, $fields = null) {
-        $form           = parent::getEditForm($id, $fields);
-        $sortable_field = $this->stat('sortable_field');
-        if (class_exists('GridFieldSortableRows') &&
-            !empty($sortable_field)) {
-            $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
-            $gridFieldConfig = $gridField->getConfig();
-            $gridFieldConfig->addComponent(new GridFieldSortableRows($sortable_field));
-        }
-        return $form;
-    }
 }
 
