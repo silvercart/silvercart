0846f2f550ea7cbb5ff054b0bc92808bd5f72bc1
Sascha Koehler <skoehler@pixeltricks.de>
2013-05-08 16:55:19 +0200

    MINOR Added SilvercartGridFieldConfig_HasOnlyOneEditor

--- admin/code/forms/gridfield/SilvercartGridFieldConfig.php
+++ admin/code/forms/gridfield/SilvercartGridFieldConfig.php
@@ -207,3 +207,50 @@ class SilvercartGridFieldConfig_Readonly extends GridFieldConfig {
     }
 
 }
+
+/**
+ * Similar to {@link GridFieldConfig_RelationEditor}, but allows add buttons
+ * only when there's no related component already.
+ *
+ * @package Silvercart
+ * @subpackage Forms_GridField
+ * @author Sebastian Diel <sdiel@pixeltricks.de>
+ * @copyright 2013 pixeltricks GmbH
+ * @since 12.02.2013
+ * @license see license file in modules root directory
+ */
+class SilvercartGridFieldConfig_HasOnlyOneEditor extends GridFieldConfig {
+
+    /**
+     * Loads the components, sets default properties.
+     *
+     * @param int $itemsPerPage How many items per page should show up
+     *
+     * @return void
+     *
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 12.02.2013
+     */
+    public function __construct($itemsPerPage = null) {
+        $this->addComponent(new GridFieldButtonRow('before'));
+        $this->addComponent(new SilvercartGridFieldDataColumns());
+        $this->addComponent(new GridFieldEditButton());
+        $this->addComponent(new GridFieldDeleteAction(true));
+        $this->addComponent(new GridFieldDetailForm());
+    }
+
+    /**
+     * Enables the add buttons for relations.
+     *
+     * @return void
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since  2013-05-08
+     */
+    public function enableAddSection() {
+        $autoCompleter = new SilvercartGridFieldAddExistingAutocompleter('buttons-before-left');
+
+        $this->addComponent(new GridFieldAddNewButton('buttons-before-left'));
+        $this->addComponent($autoCompleter);
+    }
+}
