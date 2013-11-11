9be0bbb59c8e2944243c930298f7eeae69e62398
Sebastian Diel <sdiel@pixeltricks.de>
2013-11-11 10:40:51 +0100

    MINOR-ENHANCEMENT: Added memory and general progress output methods to SilvercartTask. Added method to get all files out of a directory.

--- tasks/SilvercartTask.php
+++ tasks/SilvercartTask.php
@@ -189,6 +189,58 @@ class SilvercartTask extends ScheduledTask {
         }
         $this->Log('INFO', $info);
     }
+    
+    /**
+     * Prints the given progess info.
+     * 
+     * @param string $progress Progress to print
+     * 
+     * @return void
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 18.12.2012
+     */
+    public function printProgressInfo($progress) {
+        if (!$this->isInSilentMode()) {
+            $tab        = "\t";
+            $infoText   = $tab . "\033[33m" . $progress . "\033[0m" . "\r";
+            print $infoText;
+        }
+    }
+    
+    /**
+     * Prints the current memory usage.
+     * 
+     * @return void
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 18.12.2012
+     */
+    public function printMemoryInfo() {
+        if (!$this->isInSilentMode()) {
+            $tab        = "\t";
+            $memUsage   = 'Current Memory Usage: ' . SilvercartDebugHelper::getCurrentMemoryUsage() . ' (' . SilvercartDebugHelper::getCurrentMemoryUsage(true) . ')';
+            $infoText   = $tab . "\033[35m" . $memUsage . "\033[0m" . PHP_EOL;
+            print $infoText;
+        }
+    }
+    
+    /**
+     * Prints the current memory usage as progress.
+     * 
+     * @return void
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 18.12.2012
+     */
+    public function printMemoryProgressInfo() {
+        if (!$this->isInSilentMode()) {
+            $tab        = "\t";
+            $memUsage   = 'Current Memory Usage: ' . SilvercartDebugHelper::getCurrentMemoryUsage() . ' (' . SilvercartDebugHelper::getCurrentMemoryUsage(true) . ')';
+            $infoText   = $tab . "\033[35m" . $memUsage . "\033[0m" . "\r";
+            print $infoText;
+        }
+    }
 
     /**
      * Sets the CLI argument with the given name
@@ -356,5 +408,41 @@ class SilvercartTask extends ScheduledTask {
         $processState = null;
         exec("ps $PID", $processState);
         return count($processState) >= 2;
+    }
+    
+    /**
+     * Returns an array with all filepaths of the given file types in the given
+     * directory.
+     *
+     * @param string $directory The directory to check.
+     * @param string $extension The file extension to check for.
+     *
+     * @return array
+     */
+    protected function getFilesInDirectory($directory, $extension) {
+        $matchingFiles = array();
+        
+        if ($directory) {
+            $files = scandir($directory);
+
+            if ($files) {
+                if (strpos(strrev($directory), '/') !== 0) {
+                    $directory .= '/';
+                }
+                foreach ($files as $fileName) {
+                    $filePath = $directory . $fileName;
+
+                    if (!is_file($filePath)) {
+                        continue;
+                    }
+
+                    if (strtolower(substr($fileName, -(strlen($extension) + 1))) == '.' . strtolower($extension)) {
+                        $matchingFiles[] = $filePath;
+                    }
+                }
+            }
+        }
+
+        return $matchingFiles;
     }
 }
