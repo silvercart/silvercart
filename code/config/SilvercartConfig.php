8390dd88ba752c4004c23bd2a92fc2e0e5130849
Sebastian Diel <sdiel@pixeltricks.de>
2013-11-16 17:11:36 +0100

    MINOR: increased SilverCart version

--- code/config/SilvercartConfig.php
+++ code/config/SilvercartConfig.php
@@ -155,7 +155,7 @@ class SilvercartConfig extends DataObject {
      */
     public static $defaults = array(
         'SilvercartVersion'             => '1.3',
-        'SilvercartMinorVersion'        => '9',
+        'SilvercartMinorVersion'        => '10',
         'SilvercartUpdateVersion'       => '9',
         'DefaultPriceType'              => 'gross',
         'GeoNamesActive'                => false,
