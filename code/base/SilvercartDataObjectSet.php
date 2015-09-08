9a12863812ff3afc1fbdc508e620fba61cb1bf03
Sebastian Diel <sdiel@pixeltricks.de>
2015-09-08 10:46:56 +0200

    MINOR-BUGFIX: Fixed product pagination after refreshing a product list by ajax action.

--- code/base/SilvercartDataObjectSet.php
+++ code/base/SilvercartDataObjectSet.php
@@ -47,7 +47,7 @@ class SilvercartDataObjectSet extends DataObjectDecorator {
         if (is_null($displayedPages)) {
             $displayedPages = SilvercartConfig::DisplayedPaginationPages();
         }
-        return $this->owner->PaginationSummary($displayedPages);
+        return $this->CtrlPaginationSummary($displayedPages);
     }
 
     /**
@@ -61,5 +61,168 @@ class SilvercartDataObjectSet extends DataObjectDecorator {
     public function resetItemIndexes() {
         $this->owner->items = array_values($this->owner->items);
     }
-    
+
+    /**
+     * Returns the URL of the previous page.
+     * 
+     * @return string
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 07.09.2015
+     */
+    public function CtrlPrevLink() {
+        if ($this->owner->pageStart - $this->owner->pageLength >= 0) {
+            return HTTP::setGetVar($this->owner->paginationGetVar, $this->owner->pageStart - $this->owner->pageLength, Director::makeRelative(Controller::curr()->Link()));
+        }
+    }
+
+    /**
+     * Returns the URL of the next page.
+     * 
+     * @return string
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 07.09.2015
+     */
+    public function CtrlNextLink() {
+        if ($this->owner->pageStart + $this->owner->pageLength < $this->owner->totalSize) {
+            return HTTP::setGetVar($this->owner->paginationGetVar, $this->owner->pageStart + $this->owner->pageLength, Director::makeRelative(Controller::curr()->Link()));
+        }
+    }
+
+    /**
+     * Return a datafeed of page-links, good for use in search results, etc.
+     * $maxPages will put an upper limit on the number of pages to return.  It will
+     * show the pages surrounding the current page, so you can still get to the deeper pages.
+     * 
+     * @param int $maxPages The maximum number of pages to return
+     * 
+     * @return DataObjectSet
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 07.09.2015
+     */
+    public function CtrlPages($maxPages = 0) {
+        $ret = new DataObjectSet();
+
+        if ($maxPages) {
+            $startPage = ($this->owner->CurrentPage() - floor($maxPages / 2)) - 1;
+            $endPage = $this->owner->CurrentPage() + floor($maxPages / 2);
+
+            if ($startPage < 0) {
+                $startPage = 0;
+                $endPage = $maxPages;
+            }
+            if ($endPage > $this->owner->TotalPages()) {
+                $endPage = $this->owner->TotalPages();
+                $startPage = max(0, $endPage - $maxPages);
+            }
+
+        } else {
+            $startPage = 0;
+            $endPage = $this->owner->TotalPages();
+        }
+
+        for ($i=$startPage; $i < $endPage; $i++) {
+            $link = HTTP::setGetVar($this->owner->paginationGetVar, $i*$this->owner->pageLength, Director::makeRelative(Controller::curr()->Link()));
+            $thePage = new ArrayData(array(
+                    "PageNum" => $i+1,
+                    "Link" => $link,
+                    "CurrentBool" => ($this->owner->CurrentPage() == $i+1)?true:false,
+                    )
+            );
+            $ret->push($thePage);
+        }
+
+        return $ret;
+    }
+
+    /**
+	 * Display a summarized pagination which limits the number of pages shown
+	 * "around" the currently active page for visual balance.
+	 * In case more paginated pages have to be displayed, only 
+	 * 
+	 * Example: 25 pages total, currently on page 6, context of 4 pages
+	 * [prev] [1] ... [4] [5] [[6]] [7] [8] ... [25] [next]
+	 * 
+	 * Example template usage:
+	 * <code>
+	 * <% if MyPages.MoreThanOnePage %>
+	 * 	<% if MyPages.NotFirstPage %>
+	 * 		<a class="prev" href="$MyPages.PrevLink">Prev</a>
+	 * 	<% end_if %>
+	 *  <% control MyPages.PaginationSummary(4) %>
+	 * 		<% if CurrentBool %>
+	 * 			$PageNum
+	 * 		<% else %>
+	 * 			<% if Link %>
+	 * 				<a href="$Link">$PageNum</a>
+	 * 			<% else %>
+	 * 				...
+	 * 			<% end_if %>
+	 * 		<% end_if %>
+	 * 	<% end_control %>
+	 * 	<% if MyPages.NotLastPage %>
+	 * 		<a class="next" href="$MyPages.NextLink">Next</a>
+	 * 	<% end_if %>
+	 * <% end_if %>
+	 * </code>
+	 * 
+	 * @param integer $context Number of pages to display "around" the current page. Number should be even,	because its halved to either side of the current page.
+     * 
+	 * @return 	DataObjectSet
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 07.09.2015
+	 */
+    public function CtrlPaginationSummary($context = 4) {
+        $ret = new DataObjectSet();
+
+        // convert number of pages to even number for offset calculation
+        if ($context % 2) {
+            $context--;
+        }
+
+        // find out the offset
+        $current = $this->CurrentPage();
+        $totalPages = $this->TotalPages();
+
+        // if the first or last page is shown, use all content on one side (either left or right of current page)
+        // otherwise half the number for usage "around" the current page
+        $offset = ($current == 1 || $current == $totalPages) ? $context : floor($context/2);
+
+        $leftOffset = $current - ($offset);
+        if ($leftOffset < 1) {
+            $leftOffset = 1;
+        }
+        if ($leftOffset + $context > $totalPages) {
+            $leftOffset = $totalPages - $context;
+        }
+
+        for ($i=0; $i < $totalPages; $i++) {
+            $link = HTTP::setGetVar($this->paginationGetVar, $i*$this->pageLength, Director::makeRelative(Controller::curr()->Link()));
+            $num = $i+1;
+            $currentBool = ($current == $i+1) ? true:false;
+            if (
+                ($num == $leftOffset-1 && $num != 1 && $num != $totalPages)
+                || ($num == $leftOffset+$context+1 && $num != 1 && $num != $totalPages)
+            ) {
+                $ret->push(new ArrayData(array(
+                        "PageNum" => null,
+                        "Link" => null,
+                        "CurrentBool" => $currentBool,
+                    )
+                ));
+            } elseif ($num == 1 || $num == $totalPages || in_array($num, range($current-$offset,$current+$offset))) { 
+                $ret->push(new ArrayData(array(
+                        "PageNum" => $num,
+                        "Link" => $link,
+                        "CurrentBool" => $currentBool,
+                    )
+                ));
+            }
+        }
+        return $ret;
+    }
+
 }
