251c2f511d3a5191294d3108fb5fda9fad591eff
Sebastian Diel <sdiel@pixeltricks.de>
2018-08-21 10:00:57 +0200

    MINOR-ENHANCEMENT: Moved some methods out of the page controllers into the page itself to provide the correct page context access when needed in a different controller context for PaymentMethodsPage and ShippingFeesPage. The controllers for these pages are no more needed and deleted.

--- src/Model/Pages/PaymentMethodsPage.php
+++ src/Model/Pages/PaymentMethodsPage.php
@@ -3,7 +3,11 @@
 namespace SilverCart\Model\Pages;
 
 use SilverCart\Dev\Tools;
+use SilverCart\Model\Customer\Country;
+use SilverCart\Model\Customer\Customer;
+use SilverCart\Model\Order\ShoppingCart;
 use SilverCart\Model\Pages\MetaNavigationHolder;
+use SilverCart\Model\Payment\PaymentMethod;
 
 /**
  * Page to display available payment methods.
@@ -15,22 +19,20 @@ use SilverCart\Model\Pages\MetaNavigationHolder;
  * @copyright 2017 pixeltricks GmbH
  * @license see license file in modules root directory
  */
-class PaymentMethodsPage extends MetaNavigationHolder {
-
+class PaymentMethodsPage extends MetaNavigationHolder
+{
     /**
      * DB table name
      *
      * @var string
      */
     private static $table_name = 'SilvercartPaymentMethodsPage';
-    
     /**
      * Allowed children
      *
      * @var array
      */
     private static $allowed_children = 'none';
-    
     /**
      * Page type icon
      *
@@ -46,7 +48,8 @@ class PaymentMethodsPage extends MetaNavigationHolder {
      * @author Sebastian Diel <sdiel@pixeltricks.de>
      * @since 14.05.2012
      */
-    public function singular_name() {
+    public function singular_name()
+    {
         return Tools::singular_name_for($this);
     }
     
@@ -58,7 +61,47 @@ class PaymentMethodsPage extends MetaNavigationHolder {
      * @author Sebastian Diel <sdiel@pixeltricks.de>
      * @since 14.05.2012
      */
-    public function plural_name() {
+    public function plural_name()
+    {
         return Tools::plural_name_for($this);
+    }
+
+    /**
+     * Returns all payment methods
+     *
+     * @return DataList
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 14.05.2012
+     */
+    public function PaymentMethods()
+    {
+        return PaymentMethod::getAllowedPaymentMethodsFor($this->ShippingCountry(), ShoppingCart::singleton(), true);
+    }
+    
+    /**
+     * Returns the current shipping country
+     *
+     * @return Country
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 15.11.2014
+     */
+    public function ShippingCountry()
+    {
+        $customer        = Customer::currentUser();
+        $shippingCountry = null;
+        if ($customer) {
+            $shippingCountry = $customer->ShippingAddress()->Country();
+        }
+        if (is_null($shippingCountry)
+            || $shippingCountry->ID == 0
+        ) {
+            $shippingCountry = Country::get()->filter([
+                'ISO2'   => substr(Tools::current_locale(), 3),
+                'Active' => 1,
+            ])->first();
+        }
+        return $shippingCountry;
     }
 }
--- src/Model/Pages/ShippingFeesPage.php
+++ src/Model/Pages/ShippingFeesPage.php
@@ -4,6 +4,7 @@ namespace SilverCart\Model\Pages;
 
 use SilverCart\Dev\Tools;
 use SilverCart\Model\Pages\MetaNavigationHolder;
+use SilverCart\Model\Shipment\Carrier;
 
 /**
  * shows the shipping fee matrix.
@@ -15,22 +16,20 @@ use SilverCart\Model\Pages\MetaNavigationHolder;
  * @copyright 2017 pixeltricks GmbH
  * @license see license file in modules root directory
  */
-class ShippingFeesPage extends MetaNavigationHolder {
-
+class ShippingFeesPage extends MetaNavigationHolder
+{
     /**
      * DB table name
      *
      * @var string
      */
     private static $table_name = 'SilvercartShippingFeesPage';
-
     /**
      * allowed children on site tree
      *
      * @var array
      */
     private static $allowed_children = 'none';
-    
     /**
      * We set a custom icon for this page type here
      *
@@ -47,7 +46,8 @@ class ShippingFeesPage extends MetaNavigationHolder {
      * @author Roland Lehmann <rlehmann@pixeltricks.de>
      * @since 13.07.2012
      */
-    public function singular_name() {
+    public function singular_name()
+    {
         return Tools::singular_name_for($this);
     }
 
@@ -61,7 +61,22 @@ class ShippingFeesPage extends MetaNavigationHolder {
      * @author Roland Lehmann <rlehmann@pixeltricks.de>
      * @since 13.07.2012
      */
-    public function plural_name() {
+    public function plural_name()
+    {
         return Tools::plural_name_for($this); 
+    }
+
+    /**
+     * get all carriers; for the frontend
+     *
+     * @return DataList all carriers or empty DataList
+     * 
+     * @since 18.11.10
+     * @author Roland Lehmann <rlehmann@pixeltricks.de>
+     */
+    public function Carriers()
+    {
+        $carriers = Carrier::get()->sort(['priority' => 'DESC']);
+        return $carriers;
     }
 }
