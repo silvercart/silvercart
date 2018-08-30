1d7a1c71cd2a19080049dfac474b854c4f165df8
Sebastian Diel <sdiel@pixeltricks.de>
2018-08-30 09:46:08 +0200

    MINOR-BUGFIX: Requirements_Backend::includeInHTML() is now fully overwritten to remove the type="application/javascript" tag to be able to provide valid HTML code.

--- src/View/Requirements_Backend.php
+++ src/View/Requirements_Backend.php
@@ -3,6 +3,8 @@
 namespace SilverCart\View;
 
 use SilverStripe\Core\Manifest\ModuleResourceLoader;
+use SilverStripe\Dev\Deprecation;
+use SilverStripe\View\HTML;
 use SilverStripe\View\Requirements_Backend as SilverStripeRequirements_Backend;
 
 /**
@@ -21,7 +23,115 @@ use SilverStripe\View\Requirements_Backend as SilverStripeRequirements_Backend;
  * @since 08.05.2018
  * @license see license file in modules root directory
  */
-class Requirements_Backend extends SilverStripeRequirements_Backend {
+class Requirements_Backend extends SilverStripeRequirements_Backend
+{
+    /**
+     * -------------------------------------------------------------------------
+     * -------------------------------------------------------------------------
+     * 
+     * <b>EDIT:</b><br/>
+     * Removes the <i>type="application/javascript"</i> HTML attribute from
+     * JavaScript tags to provide valid HTML code.
+     * 
+     * -------------------------------------------------------------------------
+     * -------------------------------------------------------------------------
+     * 
+     * Update the given HTML content with the appropriate include tags for the registered
+     * requirements. Needs to receive a valid HTML/XHTML template in the $content parameter,
+     * including a head and body tag.
+     *
+     * @param string $content HTML content that has already been parsed from the $templateFile
+     *                        through {@link SSViewer}
+     * 
+     * @return string HTML content augmented with the requirements tags
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 30.08.2018
+     */
+    public function includeInHTML($content)
+    {
+        if (func_num_args() > 1) {
+            Deprecation::notice(
+                '5.0',
+                '$templateFile argument is deprecated. includeInHTML takes a sole $content parameter now.'
+            );
+            $content = func_get_arg(1);
+        }
+
+        // Skip if content isn't injectable, or there is nothing to inject
+        $tagsAvailable = preg_match('#</head\b#', $content);
+        $hasFiles = $this->css || $this->javascript || $this->customCSS || $this->customScript || $this->customHeadTags;
+        if (!$tagsAvailable || !$hasFiles) {
+            return $content;
+        }
+        $requirements = '';
+        $jsRequirements = '';
+
+        // Combine files - updates $this->javascript and $this->css
+        $this->processCombinedFiles();
+
+        // Script tags for js links
+        foreach ($this->getJavascript() as $file => $attributes) {
+            // Build html attributes
+            $htmlAttributes = [
+                'src' => $this->pathForFile($file),
+            ];
+            if (isset($attributes['type'])) {
+                $htmlAttributes['type'] = $attributes['type'];
+            }
+            if (!empty($attributes['async'])) {
+                $htmlAttributes['async'] = 'async';
+            }
+            if (!empty($attributes['defer'])) {
+                $htmlAttributes['defer'] = 'defer';
+            }
+            $jsRequirements .= HTML::createTag('script', $htmlAttributes);
+            $jsRequirements .= "\n";
+        }
+
+        // Add all inline JavaScript *after* including external files they might rely on
+        foreach ($this->getCustomScripts() as $script) {
+            $jsRequirements .= HTML::createTag('script', [], "//<![CDATA[\n{$script}\n//]]>");
+            $jsRequirements .= "\n";
+        }
+
+        // CSS file links
+        foreach ($this->getCSS() as $file => $params) {
+            $htmlAttributes = [
+                'rel' => 'stylesheet',
+                'type' => 'text/css',
+                'href' => $this->pathForFile($file),
+            ];
+            if (!empty($params['media'])) {
+                $htmlAttributes['media'] = $params['media'];
+            }
+            $requirements .= HTML::createTag('link', $htmlAttributes);
+            $requirements .= "\n";
+        }
+
+        // Literal custom CSS content
+        foreach ($this->getCustomCSS() as $css) {
+            $requirements .= HTML::createTag('style', ['type' => 'text/css'], "\n{$css}\n");
+            $requirements .= "\n";
+        }
+
+        foreach ($this->getCustomHeadTags() as $customHeadTag) {
+            $requirements .= "{$customHeadTag}\n";
+        }
+
+        // Inject CSS  into body
+        $content = $this->insertTagsIntoHead($requirements, $content);
+
+        // Inject scripts
+        if ($this->getForceJSToBottom()) {
+            $content = $this->insertScriptsAtBottom($jsRequirements, $content);
+        } elseif ($this->getWriteJavascriptToBody()) {
+            $content = $this->insertScriptsIntoBody($jsRequirements, $content);
+        } else {
+            $content = $this->insertTagsIntoHead($jsRequirements, $content);
+        }
+        return $content;
+    }
     
     /**
      * -------------------------------------------------------------------------
