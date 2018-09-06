7e55749d182b65179634d0a4a81517f4e941726a
Sebastian Diel <sdiel@pixeltricks.de>
2018-09-06 22:24:53 +0200

    MINOR-ENHANCEMENT: Added trait ExtensibleDataObject to add some advanced extensible methods to a DataObject.

---
+++ src/ORM/ExtensibleDataObject.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SilverCart\ORM;
+
+/**
+ * Trait to add extende Extensible features to a DataObject.
+ * 
+ * @package SilverCart
+ * @subpackage ORM
+ * @author Sebastian Diel <sdiel@pixeltricks.de>
+ * @since 05.09.2018
+ * @copyright 2018 pixeltricks GmbH
+ * @license see license file in modules root directory
+ */
+trait ExtensibleDataObject
+{
+    /**
+     * Allows user code to hook into DataObject::fieldLabels() prior to 
+     * updateFieldLabels being called on extensions.
+     *
+     * @param callable $callback The callback to execute
+     * 
+     * @return void
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 05.09.2018
+     */
+    protected function beforeUpdateFieldLabels($callback) {
+        $this->beforeExtending('updateFieldLabels', $callback);
+    }
+}
