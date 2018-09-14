145d898ae4a3a10dd453034eb5303477b2b42abf
Sebastian Diel <sdiel@pixeltricks.de>
2018-09-14 08:19:47 +0200

    ENHANCEMENT: Added new OrderAdmin tabs to have a fast way to filter orders by important order and payment status.

--- src/Admin/Controllers/OrderAdmin.php
+++ src/Admin/Controllers/OrderAdmin.php
@@ -3,7 +3,11 @@
 namespace SilverCart\Admin\Controllers;
 
 use SilverCart\Admin\Controllers\ModelAdmin;
+use SilverCart\Dev\Tools;
 use SilverCart\Model\Order\Order;
+use SilverCart\Model\Order\OrderStatus;
+use SilverCart\Model\Payment\PaymentStatus;
+use SilverStripe\View\ArrayData;
 use SilverStripe\View\Requirements;
 
 /**
@@ -16,38 +20,44 @@ use SilverStripe\View\Requirements;
  * @since 22.09.2017
  * @license see license file in modules root directory
  */
-class OrderAdmin extends ModelAdmin {
-
+class OrderAdmin extends ModelAdmin
+{
+    const SESSION_KEY     = 'SilverCart.OrderAdmin';
+    const SESSION_KEY_TAB = 'SilverCart.OrderAdmin.Tab';
+    
     /**
      * The code of the menu under which this admin should be shown.
      * 
      * @var string
      */
     private static $menuCode = 'orders';
-
     /**
      * The section of the menu under which this admin should be grouped.
      * 
      * @var string
      */
     private static $menuSortIndex = 10;
-
     /**
      * The URL segment
      *
      * @var string
      */
     private static $url_segment = 'silvercart-orders';
-
     /**
      * Managed models
      *
      * @var array
      */
-    private static $managed_models = array(
+    private static $managed_models = [
         Order::class,
-    );
-    
+    ];
+    /**
+     * Current tab
+     *
+     * @var string
+     */
+    protected $currentTab = null;
+
     /**
      * Provides hook for decorators, so that they can overwrite css
      * and other definitions.
@@ -60,7 +70,8 @@ class OrderAdmin extends ModelAdmin {
      *         Sascha Koehler <skoehler@pixeltricks.de>
      * @since 20.04.2018
      */
-    protected function init($skipUpdateInit = false) {
+    protected function init($skipUpdateInit = false)
+    {
         parent::init(true);
         
         Requirements::javascript('silvercart/silvercart:client/admin/javascript/jquery-ui/jquery.ui.datepicker.js');
@@ -69,8 +80,7 @@ class OrderAdmin extends ModelAdmin {
         Requirements::css('silvercart/silvercart:client/admin/css/jquery-ui/daterangepicker.css');
         
         Requirements::customScript(
-                sprintf(
-                        "
+                sprintf("
             (function($) {
                 $(document).ready(function() { 
                     //Date picker
@@ -116,4 +126,109 @@ class OrderAdmin extends ModelAdmin {
         $this->extend('updateInit');
     }
     
+    /**
+     * Returns the current model context list.
+     * Adds a filter by order or payment status if necessary by the chosen tab.
+     * 
+     * @return \SilverCart\ORM\DataList
+     */
+    public function getList()
+    {
+        return $this->getStatusList($this->getCurrentTab());
+    }
+    
+    /**
+     * Adds a filter by order or payment status dependent on the given tab.
+     * 
+     * @param string $tab Tab
+     * 
+     * @return \SilverCart\ORM\DataList
+     */
+    protected function getStatusList($tab)
+    {
+        $list = parent::getList();
+        if (!is_null($tab)
+         && $tab !== 'all'
+        ) {
+            list($statusType, $statusCode) = explode('-', $tab);
+            if ($statusType === 'order') {
+                $orderStatus = OrderStatus::get()->filter('Code', $statusCode)->first();
+                if ($orderStatus instanceof OrderStatus
+                 && $orderStatus->exists()
+                ) {
+                    $list = $list->filter('OrderStatusID', $orderStatus->ID);
+                }
+            } elseif ($statusType === 'payment') {
+                $paymentStatus = PaymentStatus::get()->filter('Code', $statusCode)->first();
+                if ($paymentStatus instanceof PaymentStatus
+                 && $paymentStatus->exists()
+                ) {
+                    $list = $list->filter('PaymentStatusID', $paymentStatus->ID);
+                }
+            }
+        }
+        return $list;
+    }
+    
+    /**
+     * Adds some additional order tabs to have a fast way to filter orders by
+     * important order or payment status.
+     * 
+     * @return \SilverStripe\ORM\ArrayList
+     */
+    protected function getManagedModelTabs()
+    {
+        $forms = parent::getManagedModelTabs();
+        $tabs  = ['order-new', 'order-inprogress', 'payment-open'];
+        $link  = $this->Link($this->sanitiseClassName(Order::class));
+        
+        if (strpos($link, '?') === false) {
+            $link = "{$link}?tab=";
+        } else {
+            $link = "{$link}&tab=";
+        }
+        
+        foreach ($forms as $form) {
+            if ($form->ClassName === Order::class) {
+                $form->Link          = $link . 'all';
+                $form->LinkOrCurrent = (Order::class == $this->modelClass && $this->getCurrentTab() === 'all') ? 'current' : 'link';
+            }
+        }
+        
+        foreach ($tabs as $tab) {
+            list($statusType, $statusCode) = explode('-', $tab);
+            $forms->push(ArrayData::create([
+                        'Title'         => _t(Order::class . '.ModelAdminTab' . ucfirst($statusType) . ucfirst($statusCode), ucfirst($statusCode)) . " ({$this->getStatusList($tab)->count()})",
+                        'ClassName'     => Order::class,
+                        'Link'          => $link . $tab,
+                        'LinkOrCurrent' => (Order::class == $this->modelClass && $this->getCurrentTab() === $tab) ? 'current' : 'link'
+            ]));
+        }
+        
+
+        return $forms;
+    }
+    
+    /**
+     * Returns the current (tab stored in session).
+     * If given by HTTP GET parameter, the current tab will be updated.
+     * 
+     * @return string
+     */
+    protected function getCurrentTab()
+    {
+        if (is_null($this->currentTab)) {
+            $this->currentTab = $this->getRequest()->getVar('tab');
+            if (!is_null($this->currentTab)) {
+                Tools::Session()->set(self::SESSION_KEY_TAB, $this->currentTab);
+                Tools::saveSession();
+            } else {
+                $this->currentTab = Tools::Session()->get(self::SESSION_KEY_TAB);
+            }
+        }
+        if (is_null($this->currentTab)) {
+            $this->currentTab = 'all';
+        }
+        return $this->currentTab;
+    }
 }
