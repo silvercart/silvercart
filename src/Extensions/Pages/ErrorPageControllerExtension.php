045d54233c7fa5493667fabac02d4bfedbe1fe51
Sebastian Diel <sdiel@pixeltricks.de>
2018-09-19 14:57:18 +0200

    ENHANCEMENT: Added extension for ErrorPageController to detect deprecated product detail links when a 404 error page is triggered. If a valid product ID is detected, a redirect to the product detail page will be forced instead of responding with the 404 error.

---
+++ src/Extensions/Pages/ErrorPageControllerExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SilverCart\Extensions\Pages;
+
+use SilverCart\Dev\Tools;
+use SilverCart\Model\Product\Product;
+use SilverStripe\Core\Extension;
+
+/**
+ * Extension for ErrorPageController.
+ * Checks the invalid URL for product ID hints. If a product ID was found, the
+ * customer will be redirected to the product detail page.
+ * 
+ * @package SilverCart
+ * @subpackage Extensions_Pages
+ * @author Sebastian Diel <sdiel@pixeltricks.de>
+ * @since 19.09.2018
+ * @copyright 2018 pixeltricks GmbH
+ * @license see license file in modules root directory
+ */
+class ErrorPageControllerExtension extends Extension
+{
+    /**
+     * Checks if the requested URL contains a product ID as the second last part.
+     * If there is a match, a redirect to the product detail page will be forced.
+     * 
+     * @return void
+     * 
+     * @author Sebastian Diel <sdiel@pixeltricks.de>
+     * @since 19.09.2018
+     */
+    public function onAfterInit()
+    {
+        if ($this->owner->data()->ErrorCode === "404"
+         && array_key_exists('url', $_GET)
+        ) {
+            $urlParts = explode('/', $_GET['url']);
+            array_pop($urlParts);
+            $productID = array_pop($urlParts);
+            if (is_numeric($productID)) {
+                $product = Product::get()->byID((int) $productID);
+                if ($product instanceof Product
+                 && $product->exists()
+                ) {
+                    Tools::redirectPermanentlyTo($product->Link());
+                }
+            }
+        }
+    }
+}
