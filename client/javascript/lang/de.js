0e3f837c424235a4c6f5f2b29fc7e649b483a7c2
Sebastian Diel <sdiel@pixeltricks.de>
2019-03-21 00:12:55 +0100

    MINOR: Added some i18n JS.

--- client/javascript/lang/de.js
+++ client/javascript/lang/de.js
@@ -11,6 +11,8 @@ if(typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
         'SilvercartGridFieldBatchController.BATCH_FAILED':      'Aktion fehlgeschlagen!',
         'SilvercartGridFieldBatchController.NO_ENTRY_SELECTED': 'Keine Objekte ausgewählt! Bitte wählen Sie mindestens ein Objekt aus der Liste aus.',
         'SilvercartGridFieldBatchController.NO_ACTION_SELECTED':'Keine Aktion ausgewählt! Bitte wählen Sie eine Aktion aus der Liste aus.',
-        'TABLEFIELD.DELETECONFIRMMESSAGE':                      'Sind Sie sicher, dass Sie diesen Eintrag löschen wollen?'
+        'TABLEFIELD.DELETECONFIRMMESSAGE':                      'Sind Sie sicher, dass Sie diesen Eintrag löschen wollen?',
+        'SilvercartProduct.ADD_TO_CART':                        'In den Warenkorb',
+        'SilvercartProduct.CHANGE_QUANTITY_CART':               'Menge ändern'
     });
 }
--- client/javascript/lang/en.js
+++ client/javascript/lang/en.js
@@ -11,6 +11,8 @@ if(typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
         'SilvercartGridFieldBatchController.BATCH_FAILED':      'Batch action failed!',
         'SilvercartGridFieldBatchController.NO_ENTRY_SELECTED': 'No objects selected! Please select at least one object entry.',
         'SilvercartGridFieldBatchController.NO_ACTION_SELECTED':'No action selected! Please select an action to execute.',
-        'TABLEFIELD.DELETECONFIRMMESSAGE':                      'Are you sure you want to delete this entry?'
+        'TABLEFIELD.DELETECONFIRMMESSAGE':                      'Are you sure you want to delete this entry?',
+        'SilvercartProduct.ADD_TO_CART':                        'Add to cart',
+        'SilvercartProduct.CHANGE_QUANTITY_CART':               'Change quantity'
     });
 }
