fc461340e5ca0e64b3267015d4dab46f80ed8eb7
Sebastian Diel <sdiel@pixeltricks.de>
2019-05-17 17:34:46 +0200

    MINOR: Updated OrderHolderController::detail(). Updated code to PSR-2.

--- src/Model/Pages/OrderHolderController.php
+++ src/Model/Pages/OrderHolderController.php
@@ -3,6 +3,8 @@
 namespace SilverCart\Model\Pages;
 
 use SilverCart\Model\Order\Order;
+use SilverCart\Model\Order\OrderInvoiceAddress;
+use SilverCart\Model\Order\OrderShippingAddress;
 use SilverCart\Model\Pages\MyAccountHolderController;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Convert;
@@ -19,15 +21,14 @@ use SilverStripe\Security\Security;
  * @copyright 2017 pixeltricks GmbH
  * @license see license file in modules root directory
  */
-class OrderHolderController extends MyAccountHolderController {
-
+class OrderHolderController extends MyAccountHolderController
+{
     /**
      * ID of the requested order
      *
      * @var int 
      */
-    protected $orderID;
-    
+    protected $orderID = 0;
     /**
      * Allowed actions
      *
@@ -46,17 +47,23 @@ class OrderHolderController extends MyAccountHolderController {
      *         Roland Lehmann <rlehmann@pixeltricks.de>
      * @since 11.06.2018
      */
-    public function detail(HTTPRequest $request) {
+    public function detail(HTTPRequest $request)
+    {
+        $customer = Security::getCurrentUser();
+        if (!($customer instanceof Member)) {
+            // there is no logged in customer
+            $this->redirect($this->Link());
+            return;
+        }
         $orderID = $request->param('ID');
         $this->setOrderID($orderID);
         // get the order to check whether it is related to the actual customer or not.
         $order = Order::get()->byID($this->getOrderID());
         /* @var $order Order */
-        if ($order instanceof Order &&
-            $order->Member()->exists()) {
-            $customer = Security::getCurrentUser();
-            if (!($customer instanceof Member) ||
-                $order->Member()->ID != $customer->ID) {
+        if ($order instanceof Order
+         && $order->Member()->exists()
+        ) {
+            if ($order->Member()->ID != $customer->ID) {
                 // the order is not related to the customer, redirect elsewhere...
                 $this->redirect($this->Link());
             }
@@ -69,12 +76,13 @@ class OrderHolderController extends MyAccountHolderController {
     /**
      * returns a single order of a logged in member identified by url param id
      *
-     * @return Order
+     * @return Order|null
      *
      * @author Sebastian Diel <sdiel@pixeltricks.de>
      * @since 28.09.2017
      */
-    public function CustomersOrder() {
+    public function CustomersOrder() : ?Order
+    {
         return Order::get_by_customer(Convert::raw2sql($this->getOrderID()));
     }
 
@@ -83,7 +91,8 @@ class OrderHolderController extends MyAccountHolderController {
      *
      * @return int
      */
-    public function getOrderID() {
+    public function getOrderID() : int
+    {
         return $this->orderID;
     }
 
@@ -92,39 +101,41 @@ class OrderHolderController extends MyAccountHolderController {
      *
      * @param int $orderID orderID
      *
-     * @return void
+     * @return $this
      */
-    public function setOrderID($orderID) {
+    public function setOrderID(int $orderID) : OrderHolderController
+    {
         $this->orderID = $orderID;
+        return $this;
     }
 
     /**
      * Returns the link to the OrderHolderPage.
      *
      * @return string
-     *
-     * @author Sascha Koehler <skoehler@pixeltricks.de>
-     * @since 13.04.2011
      */
-    public function OrderHolderLink() {
-        return $this->Parent()->Link();
+    public function OrderHolderLink() : string
+    {
+        return (string) $this->Parent()->Link();
     }
     
     /**
      * Returns the invoice address of the customers order
      *
-     * @return OrderInvoiceAddress 
+     * @return OrderInvoiceAddress|null
      */
-    public function getInvoiceAddress() {
+    public function getInvoiceAddress() : ?OrderInvoiceAddress
+    {
         return $this->CustomersOrder()->InvoiceAddress();
     }
     
     /**
      * Returns the shipping address of the customers order
      *
-     * @return OrderShippingAddress 
+     * @return OrderShippingAddress|null
      */
-    public function getShippingAddress() {
+    public function getShippingAddress() : ?OrderShippingAddress
+    {
         return $this->CustomersOrder()->ShippingAddress();
     }
 }
