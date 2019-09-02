35d00ae201bca0d6843cd15a4fef9c3ddb59aa2e
Sebastian Diel <sdiel@pixeltricks.de>
2019-09-02 10:28:59 +0200

    MINOR-ENHANCEMENT: Added Twitter Bootstrap based SwitchField (plain).

---
+++ src/Forms/FormFields/SwitchField.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SilverCart\Forms\FormFields;
+
+use SilverStripe\Forms\CheckboxField;
+
+/** 
+ * SwitchField to use together with Twitter Bootstrap 4.3.1(+) themes.
+ *
+ * @package SilverCart
+ * @subpackage Forms\FormFields
+ * @author Sebastian Diel <sdiel@pixeltricks.de>
+ * @since 30.08.2019
+ * @copyright 2019 pixeltricks GmbH
+ * @license see license file in modules root directory
+ */
+class SwitchField extends CheckboxField
+{
+    
+}
