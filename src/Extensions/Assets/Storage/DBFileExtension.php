0962eac53ccc26702036943dee2f7caf4c5f2352
Sebastian Diel <sdiel@pixeltricks.de>
2019-12-17 12:21:29 +0100

    ENHANCEMENT: Added DBFileExtension to provide additional file context features.

---
+++ src/Extensions/Assets/Storage/DBFileExtension.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace SilverCart\Extensions\Assets\Storage;
+
+use SilverStripe\Assets\Flysystem\FlysystemAssetStore;
+use SilverStripe\ORM\DataExtension;
+
+/**
+ * Extension for SilverStripe DBFile.
+ * 
+ * @package SilverCart
+ * @subpackage Extensions\Assets\Storage
+ * @author Sebastian Diel <sdiel@pixeltricks.de>
+ * @since 17.12.2019
+ * @copyright 2019 pixeltricks GmbH
+ * @license see license file in modules root directory
+ */
+class DBFileExtension extends DataExtension
+{
+    /**
+     * Performs filename cleanup before sending it back.
+     *
+     * This name should not contain hash or variants.
+     *
+     * @param string $filename
+     * 
+     * @return string
+     * 
+     * @see FlysystemAssetStore::cleanFilename()
+     */
+    protected function cleanFilename(string $filename) : string
+    {
+        // Since we use double underscore to delimit variants, eradicate them from filename
+        return preg_replace('/_{2,}/', '_', $filename);
+    }
+
+    /**
+     * Determine if legacy filenames should be used. These do not have hash path parts.
+     *
+     * @return bool
+     * 
+     * @see FlysystemAssetStore::useLegacyFilenames()
+     */
+    protected function useLegacyFilenames() : bool
+    {
+        return FlysystemAssetStore::config()->legacy_filenames;
+    }
+
+    /**
+     * Map file tuple (hash, name, variant) to a filename to be used by flysystem
+     *
+     * The resulting file will look something like my/directory/EA775CB4D4/filename__variant.jpg
+     *
+     * @return string Adapter specific identifier for this file/version
+     * 
+     * @see FlysystemAssetStore::getFileID()
+     */
+    public function getFileID() : string
+    {
+        // Since we use double underscore to delimit variants, eradicate them from filename
+        $filename = $this->cleanFilename($this->owner->Filename);
+        $name     = basename($filename);
+
+        // Split extension
+        $extension = null;
+        if (($pos = strpos($name, '.')) !== false) {
+            $extension = substr($name, $pos);
+            $name      = substr($name, 0, $pos);
+        }
+        // Unless in legacy mode, inject hash just prior to the filename
+        if ($this->useLegacyFilenames()) {
+            $fileID = $name;
+        } else {
+            $fileID = substr($this->owner->Hash, 0, 10) . "/{$name}";
+        }
+        // Add directory
+        $dirname = ltrim(dirname($filename), '.');
+        if ($dirname) {
+            $fileID = "{$dirname}/{$fileID}";
+        }
+        // Add variant
+        if (!empty($this->owner->Variant)) {
+            $fileID .= "__{$this->owner->Variant}";
+        }
+        // Add extension
+        if ($extension) {
+            $fileID .= $extension;
+        }
+        return $fileID;
+    }
+    
+    /**
+     * Returns the file path to this file.
+     * 
+     * @return string
+     */
+    public function getFilePath() : string
+    {
+        return ASSETS_PATH . DIRECTORY_SEPARATOR . $this->getFileID();
+    }
+}
