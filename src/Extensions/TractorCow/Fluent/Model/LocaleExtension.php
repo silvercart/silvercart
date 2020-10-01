49e1da6840b6caa695f654152fb3d243d2b1c50d
Sebastian Diel <sdiel@pixeltricks.de>
2020-10-01 10:33:29 +0200

    ENHANCEMENT: If a default locale is created on dev/build while having an existing SiteTree, the SiteTree will be copied to the new default locale automatically.

--- src/Extensions/TractorCow/Fluent/Model/LocaleExtension.php
+++ src/Extensions/TractorCow/Fluent/Model/LocaleExtension.php
@@ -2,9 +2,12 @@
 
 namespace SilverCart\Extensions\TractorCow\Fluent\Model;
 
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\i18n\i18n;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Versioned\Versioned;
 use TractorCow\Fluent\Model\Locale;
+use TractorCow\Fluent\State\FluentState;
 
 /**
  * 
@@ -35,6 +38,8 @@ class LocaleExtension extends DataExtension
             $locale->URLSegment      = $locale->getLocalePrefix();
             $locale->IsGlobalDefault = true;
             $locale->write();
+            Locale::clearCached();
+            $locale->publishSiteTree();
         }
     }
 
@@ -47,5 +52,42 @@ class LocaleExtension extends DataExtension
     {
         $bits = explode('_', $this->owner->Locale);
         return array_shift($bits);
+    }
+    
+    /**
+     * Publishs the already published pages for the locale of this object starting 
+     * at the root pages (without a parent).
+     * 
+     * @return void
+     */
+    public function publishSiteTree() : void
+    {
+        $currentLocale = FluentState::singleton()->getLocale();
+        FluentState::singleton()->setLocale($this->owner->Locale);
+        $rootPages = SiteTree::get()->filter('ParentID', 0);
+        foreach ($rootPages as $page) {
+            $this->publishPageWithChildren($page);
+        }
+        FluentState::singleton()->setLocale($currentLocale);
+    }
+    
+    /**
+     * Publishs the already published pages for the locale of this object 
+     * recursively.
+     * 
+     * @param SiteTree $page Page to publish
+     * 
+     * @return void
+     */
+    public function publishPageWithChildren(SiteTree $page) : void
+    {
+        if ($page->isPublished()
+         && !$page->isPublishedInLocale($this->owner->Locale)
+        ) {
+            $page->copyVersionToStage(Versioned::DRAFT, Versioned::LIVE);
+        }
+        foreach ($page->AllChildren() as $child) {
+            $this->publishPageWithChildren($child);
+        }
     }
 }
