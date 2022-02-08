9bff28f1a42dbdbf0e86afde5d9778f63d48fd3f
Sebastian Diel <sdiel@pixeltricks.de>
2022-02-08 09:30:02 +0100

    ENHANCEMENT: Added custom "showTitle" to TextCheckboxGroupField.

--- src/Forms/FormFields/TextCheckboxGroupField.php
+++ src/Forms/FormFields/TextCheckboxGroupField.php
@@ -25,26 +25,39 @@ class TextCheckboxGroupField extends CompositeField
      * @var string
      */
     protected $schemaComponent = 'TextCheckboxGroupField';
+    /**
+     * Alternative text "displayed"
+     * 
+     * @var string
+     */
+    protected $displayedText = '';
+    /**
+     * Alternative text "not displayed"
+     * 
+     * @var string
+     */
+    protected $notDisplayedText = '';
 
     /**
      * Set the composite's title to that of the first child
      *
-     * @param string $name  Field name
-     * @param string $title Field title
+     * @param string $name      Field name
+     * @param string $title     Field title
+     * @param string $showTitle Show field title
      * 
      * @return void
-     * 
-     * @author Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 19.03.2019
      */
-    public function __construct(string $name, string $title = null)
+    public function __construct(string $name, string $title = null, string $showTitle = null)
     {
         if (!$title) {
             $title = _t(self::class . '.TitleLabel', 'Title (displayed if checked)');
         }
+        if (!$showTitle) {
+            $showTitle = _t(self::class . '.ShowTitleLabel', 'Displayed');
+        }
         $fields = [
             TextField::create($name, $title),
-            CheckboxField::create("Show{$name}", _t(self::class . '.ShowTitleLabel', 'Displayed'))
+            CheckboxField::create("Show{$name}", $showTitle)
         ];
         parent::__construct($fields);
         $this->setName($name);
@@ -68,8 +81,8 @@ class TextCheckboxGroupField extends CompositeField
             $field->fieldByName($this->getName())->Value()
         ));
 
-        $displayedText    = _t(self::class . '.Displayed', 'Displayed');
-        $notDisplayedText = _t(self::class . '.NotDisplayed', 'Not displayed');
+        $displayedText    = $this->getDisplayedText();
+        $notDisplayedText = $this->getNotDisplayedText();
 
         $showField = $field->fieldByName("Show{$this->getName()}");
         if ($showField !== null) {
@@ -80,5 +93,57 @@ class TextCheckboxGroupField extends CompositeField
         }
 
         return $field;
+    }
+    
+    /**
+     * Returns the alternative text "displayed"
+     * 
+     * @return string
+     */
+    public function getDisplayedText(): string
+    {
+        if (empty($this->displayedText)) {
+            $this->setDisplayedText(_t(self::class . '.Displayed', 'Displayed'));
+        }
+        return $this->displayedText;
+    }
+
+    /**
+     * Returns the alternative text "not displayed"
+     * 
+     * @return string
+     */
+    public function getNotDisplayedText(): string
+    {
+        if (empty($this->notDisplayedText)) {
+            $this->setNotDisplayedText(_t(self::class . '.NotDisplayed', 'Not displayed'));
+        }
+        return $this->notDisplayedText;
+    }
+
+    /**
+     * Sets the alternative text "displayed"
+     * 
+     * @param string $displayedText Alternative text
+     * 
+     * @return $this
+     */
+    public function setDisplayedText(string $displayedText) : TextCheckboxGroupField
+    {
+        $this->displayedText = $displayedText;
+        return $this;
+    }
+
+    /**
+     * Sets the alternative text "not displayed"
+     * 
+     * @param string $notDisplayedText Alternative text
+     * 
+     * @return $this
+     */
+    public function setNotDisplayedText(string $notDisplayedText) : TextCheckboxGroupField
+    {
+        $this->notDisplayedText = $notDisplayedText;
+        return $this;
     }
 }
