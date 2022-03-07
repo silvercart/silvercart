314882feb0f87eb73080a6500118c83b32259a3b
Sebastian Diel <sdiel@pixeltricks.de>
2022-03-07 16:44:42 +0100

    MINOR-ENHANCEMENT: Admin side colors of BadgeColorExtension can now be set by YAML configuration.

--- src/Extensions/Model/BadgeColorExtension.php
+++ src/Extensions/Model/BadgeColorExtension.php
@@ -40,6 +40,102 @@ class BadgeColorExtension extends DataExtension
     private static $defaults = [
         'badgeColor' => 'light',
     ];
+    /**
+     * Text color for primary.
+     * 
+     * @var string
+     */
+    private static $text_color_primary = '#ffffff';
+    /**
+     * Background color for primary.
+     * 
+     * @var string
+     */
+    private static $bg_color_primary   = '#007bff';
+    /**
+     * Text color for secondary.
+     * 
+     * @var string
+     */
+    private static $text_color_secondary = '#ffffff';
+    /**
+     * Background color for secondary.
+     * 
+     * @var string
+     */
+    private static $bg_color_secondary   = '#6c757d';
+    /**
+     * Text color for success.
+     * 
+     * @var string
+     */
+    private static $text_color_success = '#ffffff';
+    /**
+     * Background color for success.
+     * 
+     * @var string
+     */
+    private static $bg_color_success   = '#28a745';
+    /**
+     * Text color for danger.
+     * 
+     * @var string
+     */
+    private static $text_color_danger = '#ffffff';
+    /**
+     * Background color for danger.
+     * 
+     * @var string
+     */
+    private static $bg_color_danger   = '#dc3545';
+    /**
+     * Text color for warning.
+     * 
+     * @var string
+     */
+    private static $text_color_warning = '#212529';
+    /**
+     * Background color for warning.
+     * 
+     * @var string
+     */
+    private static $bg_color_warning   = '#ffc107';
+    /**
+     * Text color for info.
+     * 
+     * @var string
+     */
+    private static $text_color_info = '#ffffff';
+    /**
+     * Background color for info.
+     * 
+     * @var string
+     */
+    private static $bg_color_info   = '#17a2b8';
+    /**
+     * Text color for light.
+     * 
+     * @var string
+     */
+    private static $text_color_light = '#212529';
+    /**
+     * Background color for light.
+     * 
+     * @var string
+     */
+    private static $bg_color_light   = '#f8f9fa';
+    /**
+     * Text color for dark.
+     * 
+     * @var string
+     */
+    private static $text_color_dark = '#ffffff';
+    /**
+     * Background color for dark.
+     * 
+     * @var string
+     */
+    private static $bg_color_dark   = '#343a40';
     
     /**
      * Updates the field labels.
@@ -113,17 +209,17 @@ class BadgeColorExtension extends DataExtension
             $title = $this->owner->singular_name();
         }
         return [
-            'primary'   => Tools::string2html("<span style=\"padding: 4px 8px; color: #ffffff; background-color:#007bff\">{$title}</span>"),
-            'secondary' => Tools::string2html("<span style=\"padding: 4px 8px; color: #ffffff; background-color:#6c757d\">{$title}</span>"),
-            'success'   => Tools::string2html("<span style=\"padding: 4px 8px; color: #ffffff; background-color:#28a745\">{$title}</span>"),
-            'danger'    => Tools::string2html("<span style=\"padding: 4px 8px; color: #ffffff; background-color:#dc3545\">{$title}</span>"),
-            'warning'   => Tools::string2html("<span style=\"padding: 4px 8px; color: #212529; background-color:#ffc107\">{$title}</span>"),
-            'info'      => Tools::string2html("<span style=\"padding: 4px 8px; color: #ffffff; background-color:#17a2b8\">{$title}</span>"),
-            'light'     => Tools::string2html("<span style=\"padding: 4px 8px; color: #212529; background-color:#f8f9fa\">{$title}</span>"),
-            'dark'      => Tools::string2html("<span style=\"padding: 4px 8px; color: #ffffff; background-color:#343a40\">{$title}</span>"),
+            'primary'   => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_primary}; background-color:{$this->owner->config()->bg_color_primary}\">{$title}</span>"),
+            'secondary' => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_secondary}; background-color:{$this->owner->config()->bg_color_secondary}\">{$title}</span>"),
+            'success'   => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_success}; background-color:{$this->owner->config()->bg_color_success}\">{$title}</span>"),
+            'danger'    => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_danger}; background-color:{$this->owner->config()->bg_color_danger}\">{$title}</span>"),
+            'warning'   => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_warning}; background-color:{$this->owner->config()->bg_color_warning}\">{$title}</span>"),
+            'info'      => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_info}; background-color:{$this->owner->config()->bg_color_info}\">{$title}</span>"),
+            'light'     => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_light}; background-color:{$this->owner->config()->bg_color_light}\">{$title}</span>"),
+            'dark'      => Tools::string2html("<span style=\"padding: 4px 8px; color:{$this->owner->config()->text_color_dark}; background-color:{$this->owner->config()->bg_color_dark}\">{$title}</span>"),
         ];
     }
-    
+
     /**
      * Renders the badge to display as HTML output.
      * 
