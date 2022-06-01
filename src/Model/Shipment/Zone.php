137952431a727d610e5413a6d63c437cdf0e35aa
Sebastian Diel <sdiel@pixeltricks.de>
2022-06-01 17:02:28 +0200

    BUGFIX: Fixed GridFieldAddExistingAutocompleter issue when searching for countries in Zone CMS fields.

--- src/Model/Shipment/Zone.php
+++ src/Model/Shipment/Zone.php
@@ -2,6 +2,7 @@
 
 namespace SilverCart\Model\Shipment;
 
+use SilverCart\Admin\Forms\GridField\GridFieldAddExistingAutocompleter as SilverCartGridFieldAddExistingAutocompleter;
 use SilverCart\Dev\Tools;
 use SilverCart\Model\Customer\Country;
 use SilverCart\Model\Pages\Page;
@@ -10,8 +11,10 @@ use SilverCart\Model\Shipment\Carrier;
 use SilverCart\Model\Shipment\ShippingFee;
 use SilverCart\Model\Shipment\ShippingMethod;
 use SilverCart\Model\Shipment\ZoneTranslation;
-use SilverCart\ORM\DataObjectExtension;
 use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
+use SilverStripe\ORM\DataList;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\Filters\ExactMatchFilter;
 use SilverStripe\ORM\Filters\PartialMatchFilter;
@@ -21,54 +24,53 @@ use SilverStripe\ORM\Filters\PartialMatchFilter;
  * Every carrier might have it´s own zones. That´s why zones:countries is n:m.
  *
  * @package SilverCart
- * @subpackage Model_Shipment
+ * @subpackage Model\Shipment
  * @author Sebastian Diel <sdiel@pixeltricks.de>
  * @since 29.09.2017
  * @copyright 2017 pixeltricks GmbH
  * @license see license file in modules root directory
  */
-class Zone extends DataObject {
-    
+class Zone extends DataObject
+{
+    use \SilverCart\ORM\ExtensibleDataObject;
     /**
      * Has-many relationship.
      *
      * @var array
      */
-    private static $has_many = array(
+    private static $has_many = [
         'ShippingFees'     => ShippingFee::class,
         'ZoneTranslations' => ZoneTranslation::class,
         'HandlingCosts'    => HandlingCost::class,
-    );
+    ];
     /**
      * Many-many relationships.
      *
      * @var array
      */
-    private static $many_many = array(
-        'Countries'       => Country::class,
-        'Carriers'        => Carrier::class,
-    );
+    private static $many_many = [
+        'Countries' => Country::class,
+        'Carriers'  => Carrier::class,
+    ];
     /**
      * Belongs-many-many relationships.
      *
      * @var array
      */
-    private static $belongs_many_many = array(
+    private static $belongs_many_many = [
         'ShippingMethods' => ShippingMethod::class,
-    );
-    
+    ];
     /**
      * Virtual database columns.
      *
      * @var array
      */
-    private static $casting = array(
+    private static $casting = [
         'AttributedCountries'       => 'Varchar(255)',
         'AttributedShippingMethods' => 'Varchar(255)',
         'CarriersAsString'          => 'Text',
         'Title'                     => 'Text',
-    );
-
+    ];
     /**
      * DB table name
      *
@@ -79,59 +81,53 @@ class Zone extends DataObject {
     /**
      * Field labels for display in tables.
      *
-     * @param boolean $includerelations A boolean value to indicate if the labels returned include relation fields
+     * @param bool $includerelations A boolean value to indicate if the labels returned include relation fields
      *
      * @return array
-     *
-     * @author Roland Lehmann <rlehmann@pixeltricks.de>
-     * @since 5.7.2011
      */
-    public function fieldLabels($includerelations = true) {
-        $fieldLabels = array_merge(
-                parent::fieldLabels($includerelations),
-                array(
-                        'Title'                     => Page::singleton()->fieldLabel('Title'),
-                        'Carriers'                  => Carrier::singleton()->plural_name(),
-                        'AttributedCountries'       => _t(Zone::class . '.ATTRIBUTED_COUNTRIES', 'attributed countries'),
-                        'AttributedShippingMethods' => _t(Zone::class . '.ATTRIBUTED_SHIPPINGMETHODS', 'attributed shipping methods'),
-                        'ShippingFees'              => ShippingFee::singleton()->plural_name(),
-                        'ShippingMethods'           => ShippingMethod::singleton()->plural_name(),
-                        'Countries'                 => _t(Country::class . '.PLURALNAME', 'Countries'),
-                        'UseAllCountries'           => _t(Zone::class . '.USE_ALL_COUNTRIES', 'Relate all countries after saving'),
-                        'ZoneTranslations'          => ZoneTranslation::singleton()->plural_name(),
-                        'HandlingCosts'             => HandlingCost::singleton()->plural_name(),
-                )
-        );
-        $this->extend('updateFieldLabels', $fieldLabels);
-        return $fieldLabels;
+    public function fieldLabels($includerelations = true) : array
+    {
+        return $this->defaultFieldLabels($includerelations, [
+            'Title'                     => Page::singleton()->fieldLabel('Title'),
+            'Carriers'                  => Carrier::singleton()->plural_name(),
+            'AttributedCountries'       => _t(Zone::class . '.ATTRIBUTED_COUNTRIES', 'attributed countries'),
+            'AttributedShippingMethods' => _t(Zone::class . '.ATTRIBUTED_SHIPPINGMETHODS', 'attributed shipping methods'),
+            'ShippingFees'              => ShippingFee::singleton()->plural_name(),
+            'ShippingMethods'           => ShippingMethod::singleton()->plural_name(),
+            'Countries'                 => _t(Country::class . '.PLURALNAME', 'Countries'),
+            'UseAllCountries'           => _t(Zone::class . '.USE_ALL_COUNTRIES', 'Relate all countries after saving'),
+            'ZoneTranslations'          => ZoneTranslation::singleton()->plural_name(),
+            'HandlingCosts'             => HandlingCost::singleton()->plural_name(),
+        ]);
     }
 
     /**
      * customizes the backends fields, mainly for ModelAdmin
      *
-     * @return FieldList the fields for the backend
-     * 
-     * @author Roland Lehmann <rlehmann@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 13.02.2013
+     * @return FieldList
      */
-    public function getCMSFields() {
-        $fields = DataObjectExtension::getCMSFields($this);
-        if ($this->isInDB()) {
-            $useAllCountries = new CheckboxField('UseAllCountries', $this->fieldLabel('UseAllCountries'));
-            $fields->addFieldToTab('Root.Main', $useAllCountries);
-        }
-        return $fields;
+    public function getCMSFields() : FieldList
+    {
+        $this->beforeUpdateCMSFields(function(FieldList $fields) {
+            if ($this->isInDB()) {
+                $useAllCountries = CheckboxField::create('UseAllCountries', $this->fieldLabel('UseAllCountries'));
+                $fields->addFieldToTab('Root.Main', $useAllCountries);
+                $countryGrid       = $fields->dataFieldByName('Countries');
+                $countryGridConfig = $countryGrid->getConfig();
+                $countryGridConfig->removeComponentsByType(GridFieldAddExistingAutocompleter::class);
+                $countryGridConfig->addComponent(new SilverCartGridFieldAddExistingAutocompleter('buttons-before-right'));
+            }
+        });
+        return parent::getCMSFields();
     }
     
     /**
      * Returns the translated singular name of the object.
      * 
      * @return string
-     * 
-     * @author Roland Lehmann <rlehmann@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 21.06.2012
      */
-    public function singular_name() {
+    public function singular_name() : string
+    {
         return Tools::singular_name_for($this);
     }
     
@@ -139,11 +135,9 @@ class Zone extends DataObject {
      * Returns the translated plural name of the object.
      * 
      * @return string
-     * 
-     * @author Roland Lehmann <rlehmann@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 21.06.2012
      */
-    public function plural_name() {
+    public function plural_name() : string
+    {
         return Tools::plural_name_for($this);
     }
     
@@ -152,37 +146,36 @@ class Zone extends DataObject {
      *
      * @return string
      */
-    public function getTitle() {
-        return $this->getTranslationFieldValue('Title');
+    public function getTitle() : string
+    {
+        return (string) $this->getTranslationFieldValue('Title');
     }
     
     /**
      * Searchable fields
      *
      * @return array
-     *
-     * @author Roland Lehmann <rlehmann@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 26.04.2012
      */
-    public function searchableFields() {
-        $searchableFields = array(
-            'ZoneTranslations.Title' => array(
+    public function searchableFields() : array
+    {
+        $searchableFields = [
+            'ZoneTranslations.Title' => [
                 'title' => $this->fieldLabel('Title'),
                 'filter' => PartialMatchFilter::class,
-            ),
-            'Carriers.ID' => array(
+            ],
+            'Carriers.ID' => [
                 'title' => $this->fieldLabel('Carriers'),
                 'filter' => ExactMatchFilter::class,
-            ),
-            'Countries.ID' => array(
+            ],
+            'Countries.ID' => [
                 'title' => $this->fieldLabel('Countries'),
                 'filter' => ExactMatchFilter::class,
-            ),
-            'ShippingMethods.ID' => array(
+            ],
+            'ShippingMethods.ID' => [
                 'title' => $this->fieldLabel('ShippingMethods'),
                 'filter' => ExactMatchFilter::class,
-            )
-        );
+            ],
+        ];
         $this->extend('updateSearchableFields', $searchableFields);
         return $searchableFields;
     }
@@ -191,18 +184,15 @@ class Zone extends DataObject {
      * Summaryfields for display in tables.
      *
      * @return array
-     *
-     * @author Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 15.02.2011
      */
-    public function summaryFields() {
-        $summaryFields = array(
+    public function summaryFields() : array
+    {
+        $summaryFields = [
             'Title'                     => $this->fieldLabel('Title'),
             'CarriersAsString'          => $this->fieldLabel('Carriers'),
             'AttributedCountries'       => $this->fieldLabel('AttributedCountries'),
             'AttributedShippingMethods' => $this->fieldLabel('AttributedShippingMethods'),
-        );
-        
+        ];
         $this->extend('updateSummaryFields', $summaryFields);
         return $summaryFields;
     }
@@ -211,11 +201,9 @@ class Zone extends DataObject {
      * Processing hook before writing the DataObject
      * 
      * @return void
-     *
-     * @author Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 04.04.2012 
      */
-    protected function onBeforeWrite() {
+    protected function onBeforeWrite() : void
+    {
         parent::onBeforeWrite();
         if (array_key_exists('UseAllCountries', $_POST)) {
             $countries = Country::get();
@@ -229,24 +217,20 @@ class Zone extends DataObject {
      * Returns the attributed countries as string (limited to 150 chars).
      *
      * @return string
-     *
-     * @author Sascha Koehler <skoehler@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 05.04.2012
      */
-    public function AttributedCountries() {
-        return Tools::AttributedDataObject($this->Countries());
+    public function AttributedCountries()  : string
+    {
+        return (string) Tools::AttributedDataObject($this->Countries());
     }
 
     /**
      * Returns the attributed shipping methods as string (limited to 150 chars).
      *
      * @return string
-     *
-     * @author Sascha Koehler <skoehler@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 05.04.2012
      */
-    public function AttributedShippingMethods() {
-        return Tools::AttributedDataObject($this->ShippingMethods());
+    public function AttributedShippingMethods()  : string
+    {
+        return (string) Tools::AttributedDataObject($this->ShippingMethods());
     }
     
     /**
@@ -254,11 +238,13 @@ class Zone extends DataObject {
      *
      * @return string
      */
-    public function getCarriersAsString() {
+    public function getCarriersAsString() : string
+    {
         $carriersAsString = '---';
         $carriersAsArray  = $this->Carriers()->map()->toArray();
-        if (count($carriersAsArray) > 0 &&
-            is_array($carriersAsArray)) {
+        if (count($carriersAsArray) > 0
+         && is_array($carriersAsArray)
+        ) {
             $carriersAsString = implode(',', $carriersAsArray);
         }
         return $carriersAsString;
@@ -271,31 +257,28 @@ class Zone extends DataObject {
      * 
      * @return DataList
      */
-    public static function getZonesFor($countryID) {
-        $zoneTable    = Tools::get_table_name(Zone::class);
-        $countryTable = Tools::get_table_name(Country::class);
+    public static function getZonesFor(int $countryID) : DataList
+    {
+        $zoneTable    = self::config()->table_name;
+        $countryTable = Country::config()->table_name;
         return self::get()
             ->leftJoin(
-                $zoneTable . '_Countries',
-                'SZSC.' . $zoneTable . 'ID = ' . $zoneTable . '.ID',
+                "{$zoneTable}_Countries",
+                "SZSC.{$zoneTable}ID = {$zoneTable}.ID",
                 'SZSC'
             )
-            ->filter(
-                array(
-                    $countryTable . 'ID' => $countryID,
-                )
-            );
+            ->filter([
+                $countryTable . 'ID' => $countryID,
+            ]);
     }
     
     /**
      * Returns whether this zone is related to all active countries
      *
-     * @return boolean 
-     * 
-     * @author Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 08.06.2012
+     * @return bool
      */
-    public function hasAllCountries() {
+    public function hasAllCountries() : bool
+    {
         /* @var $countries ArrayList */
         $countries          = $this->Countries();
         $availableCountries = Country::get()->filter("Active", 1);
@@ -308,5 +291,4 @@ class Zone extends DataObject {
         }
         return $hasAllCountries;
     }
-
 }
