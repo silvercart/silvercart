8392c9b8ba7d1082fe4f2178f172e78e71d0c0fd
Jan Lehmann <jan.lehmann@gmail.com>
2022-07-07 17:20:27 +0200

    ENHANCEMENT: i18n support for brand navigation module

--- src/Model/Content/BrandNavigationItem.php
+++ src/Model/Content/BrandNavigationItem.php
@@ -4,71 +4,94 @@ namespace SilverCart\Model\Content;
 
 use Sheadawson\Linkable\Forms\LinkField;
 use Sheadawson\Linkable\Models\Link;
+use SilverCart\ORM\ExtensibleDataObject;
 use SilverStripe\Assets\File;
 use SilverStripe\Assets\Image;
+use SilverStripe\Assets\Storage\DBFile;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\HiddenField;
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\SiteConfig\SiteConfig;
 
+/**
+* @property string $Title          Title
+* @property int    $Sort           Sort
+* @property int    $LinkID         Related Link ID
+* @property int    $SiteConfigID   Related SiteConfig ID
+* @property bool   $IsActive       IsActive
+* 
+* @method Link       Link()       Returns the related Link.
+* @method SiteConfig SiteConfig() Returns the related SiteConfig.
+*/
 class BrandNavigationItem extends DataObject
 {
-
+    use ExtensibleDataObject;
     /**
      * Defines the database table name
      *  @var string
      */
     private static $table_name = 'SilverCart_Content_BrandNavigationItem';
-
     /**
      * Database fields
-     * @var array
+     * @var string[]
      */
     private static $db = [
         'Title'     => 'Varchar',
         'Sort'      => 'Int',
-        'isActive'  => 'Boolean'
+        'IsActive'  => 'Boolean'
     ];
-
     /**
      * Has_one relationship
-     * @var array
+     * @var string[]
      */
     private static $has_one = [
         'Image'      => Image::class,
         'Link'       => Link::class,
         'SiteConfig' => SiteConfig::class,
     ];
-
-
     /**
      * Defines summary fields commonly used in table columns
      * as a quick overview of the data for this dataobject
-     * @var array
+     * @var string[]
      */
     private static $summary_fields = [
-        'ThumbnailNice' => 'Image', 
-        'Title' => 'Title', 
-        'Link.LinkType' => 'Link Type',
-        'Link.LinkURL' => 'Link URL',
-        'isActive' => 'is Active'
+        'ThumbnailNice',
+        'Title',
+        'Link.LinkType',
+        'Link.LinkURL',
+        'IsActive'
     ];
-
-    public function getThumbnailNice() {
+    /**
+     * Add default values to database
+     *  @var string[]
+     */
+    private static $defaults = [
+        'IsActive' => true
+    ];
+    /**
+     * Scales Image for GridView
+     * 
+     * @return DBFile|null
+     */
+    public function getThumbnailNice() : ?DBFile
+    {
 		return $this->Image()->ScaleHeight(20);
 	}
 
     /**
-     * Add default values to database
-     *  @var array
+     * @param bool $includerelations Include relation or not?
+     * 
+     * @return string[]
      */
-    private static $defaults = [
-        'isActive' => true
-    ];
-
-    
-
+    public function fieldLabels($includerelations = true) : array
+    {
+        return $this->defaultFieldLabels($includerelations, [
+            'ThumbnailNice' => _t(self::class .".Image", "Logo"),
+            'Link.LinkType' => _t(self::class .".LinkType", "Link Type"),
+            'Link.LinkURL'  => _t(self::class .".LinkURL", "Link Url"),
+        ]);
+    }
     
     /**
      * CMS Fields
@@ -93,13 +116,4 @@ class BrandNavigationItem extends DataObject
         
         return parent::getCMSFields();
     }
-
-    
-    
-    // public function getTitle() : string
-    // {
-    //     $title = parent::getTitle();
-    //     $title = "{$this->Link} ({$this->Image})";
-    //     return $title;
-    // }
 }
