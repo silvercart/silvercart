428dba280933602c4ba83263b0ea490240370974
Sebastian Diel <sdiel@pixeltricks.de>
2022-10-17 17:41:13 +0200

    ENHANCEMENT: Added extension hooks CartPage::updateBeforeShoppingCartFull() and CartPage::updateAfterShoppingCartFull().

--- src/Model/Pages/CartPage.php
+++ src/Model/Pages/CartPage.php
@@ -3,6 +3,10 @@
 namespace SilverCart\Model\Pages;
 
 use Page;
+use SilverCart\Model\Customer\Customer;
+use SilverCart\Model\Order\ShoppingCart;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\Security\Member;
 
 /**
  * represents a shopping cart. Every customer has one initially.
@@ -28,4 +32,52 @@ class CartPage extends Page
      * @var string
      */
     private static $icon_class = 'font-icon-p-cart';
+    /**
+     * Shopping cart.
+     * 
+     * @var ShoppingCart|null
+     */
+    protected $cart = null;
+
+    /**
+     * Returns the shopping cart.
+     * 
+     * @return ShoppingCart|null
+     */
+    public function getCart() : ShoppingCart|null
+    {
+        if ($this->cart === null) {
+            $customer = Customer::currentUser();
+            if ($customer instanceof Member) {
+                $this->cart = $customer->getCart();
+            }
+        }
+        return $this->cart;
+    }
+    
+    /**
+     * Returns some additional content to insert before the ShoppingCartFull
+     * template.
+     * 
+     * @return DBHTMLText
+     */
+    public function BeforeShoppingCartFull() : DBHTMLText
+    {
+        $content = '';
+        $this->extend('updateBeforeShoppingCartFull', $content);
+        return DBHTMLText::create()->setValue($content);
+    }
+    
+    /**
+     * Returns some additional content to insert before the ShoppingCartFull
+     * template.
+     * 
+     * @return DBHTMLText
+     */
+    public function AfterShoppingCartFull() : DBHTMLText
+    {
+        $content = '';
+        $this->extend('updateAfterShoppingCartFull', $content);
+        return DBHTMLText::create()->setValue($content);
+    }
 }
