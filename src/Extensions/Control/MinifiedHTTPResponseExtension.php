4a2b19a261dbea8817993051b6c1a57778406956
Sebastian Diel <sdiel@pixeltricks.de>
2023-06-12 10:22:16 +0200

    Bugfix

--- src/Extensions/Control/MinifiedHTTPResponseExtension.php
+++ src/Extensions/Control/MinifiedHTTPResponseExtension.php
@@ -3,6 +3,7 @@
 namespace SilverCart\Extensions\Control;
 
 use SilverCart\Control\MinifiedHTTPResponse;
+use SilverCart\Dev\Tools;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Extension;
@@ -16,6 +17,8 @@ use SilverStripe\Core\Extension;
  * @since 20.09.2018
  * @copyright 2018 pixeltricks GmbH
  * @license see license file in modules root directory
+ * 
+ * @property Controller $owner Owner
  */
 class MinifiedHTTPResponseExtension extends Extension
 {
@@ -23,12 +26,12 @@ class MinifiedHTTPResponseExtension extends Extension
      * Initializes the controller with a response of type MinifiedHTTPResponse.
      * 
      * @return void
-     * 
-     * @author Sebastian Diel <sdiel@pixeltricks.de>
-     * @since 20.09.2018
      */
-    public function onBeforeInit()
+    public function onBeforeInit() : void
     {
+        if (Tools::isBackendEnvironment()) {
+            return;
+        }
         if (Director::isLive()) {
             if ($this->owner instanceof Controller) {
                 $this->owner->setResponse(MinifiedHTTPResponse::create());
